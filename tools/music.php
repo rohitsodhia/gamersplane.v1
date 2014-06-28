b5526bd0579f0261337bc098d0fd9a7c7d18ee5c
Rohit Sodhia <rohit@rhovisions.com>
2014-06-28 09:03:42 -0400

    Created music.js

--- tools/music.php
+++ tools/music.php
@@ -4,28 +4,26 @@
 ?>
 <? require_once(FILEROOT.'/header.php'); ?>
 		<h1 class="headerbar">Music and Clips</h1>
-		<div class="hbMargined">
-			<div class="sideWidget left">
-				<h2>Filter by Genre</h2>
-				<form method="get">
-					<ul class="clearfix">
+		<div class="sideWidget left">
+			<h2>Filter by Genre</h2>
+			<form method="get">
+				<ul class="clearfix">
 <?
 	$selectedGenres = array();
 	foreach (Music_consts::getGameTypes() as $type) {
 		$selectedGenre = isset($_GET['filter'], $_GET['genres']) && array_search($type, $_GET['genres']) !== FALSE?TRUE:FALSE;
 		$cleanType = preg_replace('/[^A-za-z]/', ' ', $type);
 ?>
-						<li><input id="genre_<?=$cleanType?>" type="checkbox" name="genres[<?=$type?>]"> <label for="genre_<?=$cleanType?>"><?=$type?></label>
+					<li><input id="genre_<?=$cleanType?>" type="checkbox" name="genres[<?=$type?>]"> <label for="genre_<?=$cleanType?>"><?=$type?></label>
 <?
 		if ($selectedGenre) $selectedGenres[] = $type;
 	}
 ?>
-					</ul>
-				</form>
-				<div class="alignCenter"><button name="filter" value="filter" class="fancyButton">Filter</button></div>
-			</div>
+				</ul>
+			</form>
+			<div class="alignCenter"><button name="filter" value="filter" class="fancyButton">Filter</button></div>
 		</div>
 		<div class="mainColumn right">
-			<? var_dump($selectedGenres); ?>
+			<a id="addMusic" href="/tools/music/add" class="fancyButton smallButton">Add Music</a>
 		</div>
 <? require_once(FILEROOT.'/footer.php'); ?>
