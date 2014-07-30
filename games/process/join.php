96d251193d208ecffcd96de07b57425dc938f608
Rohit Sodhia <rohit@rhovisions.com>
2014-07-30 13:33:26 -0400

    Primary GM shouldn't count as a player

--- games/process/join.php
+++ games/process/join.php
@@ -8,7 +8,7 @@
 		$numPlayers = $mysql->query("SELECT numPlayers FROM games WHERE gameID = $gameID");
 		$numPlayers = $numPlayers->fetchColumn();
 		$numApprovedPlayers = $mysql->query("SELECT u.userID FROM users u, players p WHERE p.gameID = $gameID AND u.userID = p.userID AND p.approved = 1 ORDER BY u.username ASC");
-		$numApprovedPlayers = $numApprovedPlayers->rowCount();
+		$numApprovedPlayers = $numApprovedPlayers->rowCount() - 1;
 		
 		if ($numApprovedPlayers < $numPlayers) {
 			$mysql->query("INSERT INTO players (gameID, userID) VALUES ($gameID, $userID)");
