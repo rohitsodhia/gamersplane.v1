aad652418894d12a4ee2e3ce5856707880d86be4
Rohit Sodhia <rohit@rhovisions.com>
2014-08-03 19:21:53 -0400

    Setup system char class

--- includes/characters/savageworlds/savageworldsCharacter.class.php
+++ includes/characters/savageworlds/savageworldsCharacter.class.php
@@ -2,12 +2,7 @@
 	class savageworldsCharacter extends Character {
 		const SYSTEM = 'savageworlds';
 
-		protected $stats = array(
-			'agi' => array('dice' => '', 'skills' => ''), 
-			'sma' => array('dice' => '', 'skills' => ''), 
-			'spi' => array('dice' => '', 'skills' => ''), 
-			'str' => array('dice' => '', 'skills' => ''), 
-			'vig' => array('dice' => '', 'skills' => '')
+		protected $stats = array('agi' => null, 'sma' => null, 'spi' => null, 'str' => null, 'vig' => null
 		);
 		protected $derivedStats = array('pace' => 0, 'parry' => 0, 'charisma' => 0, 'toughness' => 0);
 		protected $edgesHindrances = '';
@@ -15,24 +10,20 @@
 		protected $fatigue = 0;
 		protected $injuries = '';
 		protected $weapons = '';
-		protected $arcane = '';
 		protected $equipment = '';
 		protected $advances = '';
 
-		public function setStat($stat, $sub, $value = '') {
-			if ($sub != 'dice' && $sub != 'skills') return FALSE;
+		protected $linkedTables = array('skills');
 
-			if (array_key_exists($stat, $this->stats)) $this->stats[$stat][$sub] = $value;
+		public function setStat($stat, $value = null) {
+			if (array_key_exists($stat, $this->stats)) $this->stats[$stat] = intval($value);
 			else return FALSE;
 		}
 		
-		public function getStats($stat = NULL, $sub = NULL) {
+		public function getStats($stat = NULL) {
 			if ($stat == NULL) return $this->stats;
-			elseif (array_key_exists($stat, $this->stats) && $sub == NULL) return $this->stats[$stat];
-			elseif (array_key_exists($stat, $this->stats)) {
-				if ($sub != 'dice' && $sub != 'skills') return FALSE;
-				else return $this->stats[$stat][$sub];
-			} else return FALSE;
+			elseif (array_key_exists($stat, $this->stats)) return $this->stats;
+			else return FALSE;
 		}
 
 		public function setEdgesHindrances($edgesHindrances) {
@@ -43,31 +34,28 @@
 			return $this->edgesHindrances;
 		}
 
-		public function setNightmare($nightmare) {
-			$this->nightmare = $nightmare;
-		}
-
-		public function getNightmare() {
-			return $this->nightmare;
-		}
-
-		public function setWounds($region, $value) {
-			if (array_key_exists($region, $this->wounds)) $this->wounds[$region] = $value;
-			else return FALSE;
+		public function setWounds($value) {
+			$this->wounds = intval($wounds);
 		}
 
 		public function getWounds($region) {
-			if ($region == NULL) return $this->wounds;
-			elseif (array_key_exists($region, $this->wounds)) return $this->wounds[$region];
-			else return FALSE;
+			return $this->wounds;
 		}
 
-		public function setWind($wind) {
-			$this->wind = intval($wind);
+		public function setFatigue($fatigue) {
+			$this->fatigue = intval($fatigue);
 		}
 
-		public function getWind() {
-			return $this->wind;
+		public function getFatigue() {
+			return $this->fatigue;
+		}
+
+		public function setInjuries($injuries) {
+			$this->injuries = $injuries;
+		}
+
+		public function getInjuries() {
+			return $this->injuries;
 		}
 
 		public function setWeapons($weapons) {
@@ -78,20 +66,20 @@
 			return $this->weapons;
 		}
 
-		public function setArcane($arcane) {
-			$this->arcane = $arcane;
-		}
-
-		public function getArcane() {
-			return $this->arcane;
-		}
-
 		public function setEquipment($equipment) {
 			$this->equipment = $equipment;
 		}
 
 		public function getEquipment() {
 			return $this->equipment;
+		}
+
+		public function setAdvances($advances) {
+			$this->advances = $advances;
+		}
+
+		public function getAdvances() {
+			return $this->advances;
 		}
 
 		public function save() {
@@ -104,12 +92,12 @@
 					$this->setStat($stat, 'skills', $value['skills']);
 				}
 				$this->setEdgesHindrances($data['edgesHindrances']);
-				$this->setNightmare($data['nightmare']);
-				foreach ($data['wounds'] as $region => $value) $this->setWounds($region, $value);
-				$this->setWind($data['wind']);
+				$this->setWounds($data['wounds']);
+				$this->setFatigue($data['fatigue']);
+				$this->setInjuries($data['injuries']);
 				$this->setWeapons($data['weapons']);
-				$this->setArcane($data['arcane']);
 				$this->setEquipment($data['equipment']);
+				$this->setAdvances($data['advances']);
 			}
 
 			parent::save();
