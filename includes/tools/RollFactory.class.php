ebc5d8dc8d85f461d65401a931b024d85dbef815
Rohit Sodhia <rohit@rhovisions.com>
2014-09-19 17:17:26 -0400

    Error catching to dice

--- includes/tools/RollFactory.class.php
+++ includes/tools/RollFactory.class.php
@@ -5,6 +5,8 @@
 		private function __construct() { }
 
 		public static function getRoll($type) {
+			if (!array_key_exists($type, self::$systemMap)) return false;
+			
 			if (class_exists($type.'Roll')) $classname = $type.'Roll';
 			elseif (class_exists(self::$systemMap[$type].'Roll')) $classname = $this->systemMap[$type].'Roll';
 			else throw new Exception('Invalid type');
