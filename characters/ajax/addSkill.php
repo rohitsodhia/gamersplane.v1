6385180c8ed93427bc75b97eb58a3144e825f01f
Rohit Sodhia <rohit@rhovisions.com>
2014-09-23 19:06:40 -0400

    DnD3 updated

--- characters/ajax/addSkill.php
+++ characters/ajax/addSkill.php
@@ -5,7 +5,7 @@
 		if ($systems->getSystemID(SYSTEM)) {
 			require_once(FILEROOT.'/includes/packages/'.SYSTEM.'Character.package.php');
 			$charClass = SYSTEM.'Character';
-			$charClass::skillEditFormat(intval($_POST['key']), null, null, true);
+			$charClass::skillEditFormat(intval($_POST['key']));
 		}
 	}
 ?>
