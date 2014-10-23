f98117bb2c4263b5274f3af23e634581f77db6b7
Rohit Sodhia <rohit@rhovisions.com>
2014-10-23 17:45:41 -0400

    Cleanup header

--- header.php
+++ header.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-<? require_once(FILEROOT.'/meta.php'); ?>
+<?	require_once(FILEROOT.'/meta.php'); ?>
 
-<? require_once(FILEROOT.'/styles/styles.php'); ?>
+<?	require_once(FILEROOT.'/styles/styles.php'); ?>
 </head>
 
 <body<?=(MODAL?' class="modal"':'')?> data-modal-width="<?=$dispatchInfo['modalWidth']?>">
-<? if (!MODAL) { ?>
+<?	if (!MODAL) { ?>
 <header class="bodyHeader"><div class="bodyContainer">
 	<a href="/"><img id="logo" src="/images/bodyComponents/logo.png" alt="Gamers Plane Logo"></a>
 	
 	<div id="userMenu">
 		<div id="userMenu_left"></div>
 		<div id="userMenu_right"></div>
-<? if (!$loggedIn) { ?>
+<?		if (!$loggedIn) { ?>
 		<a href="/login/" class="loginLink first">Login</a>
 		<a href="/register/" class="last">Register</a>
 <?
-	} else {
+		} else {
 		$header_numMessages = $mysql->query('SELECT COUNT(*) FROM pms WHERE recipientID = '.intval($_SESSION['userID']).' AND viewed = 0');
 		$header_numNewMessages = $header_numMessages->fetchColumn();
 ?>
@@ -26,7 +26,7 @@
 		<a href="/pms/"><img src="/images/envelope.jpg" title="Private Messages" alt="Private Messages"> (<?=$header_numNewMessages?>)</a>
 		<a href="/notifications/"><img src="/images/bodyComponents/exclamation.jpg" title="Notifications" alt="Notifications"></a>
 		<a href="/logout/" class="last">Logout</a>
-<? } ?>
+<?		} ?>
 	</div>
 	
 	<div id="followLinks">
@@ -39,50 +39,50 @@
 	<div id="mainMenu">
 		<ul id="mainMenu_left">
 			<li><a href="/tools/" class="first">Tools</a></li>
-<? if ($loggedIn) { ?>
+<?		if ($loggedIn) { ?>
 			<li>
 				<a href="/characters/my/">Characters</a>
 <?
-		$header_characters = $mysql->query('SELECT c.characterID, c.label, s.shortName FROM characters c, systems s WHERE c.systemID = s.systemID AND c.userID = '.intval($_SESSION['userID']).' ORDER BY c.label');
-		if ($header_characters->rowCount()) {
-			echo "				<ul>\n";
-			$count = 0;
-			foreach ($header_characters as $hCharacter) {
-				if ($count > 5) {
-					echo "					".'<li><a href="/characters/my/">All characters</a></li>'."\n";
-					break;
+			$header_characters = $mysql->query('SELECT c.characterID, c.label, s.shortName FROM characters c, systems s WHERE c.systemID = s.systemID AND c.userID = '.intval($_SESSION['userID']).' ORDER BY c.label');
+			if ($header_characters->rowCount()) {
+				echo "				<ul>\n";
+				$count = 0;
+				foreach ($header_characters as $hCharacter) {
+					if ($count > 5) {
+						echo "					".'<li><a href="/characters/my/">All characters</a></li>'."\n";
+						break;
+					}
+					echo "					".'<li><a href="/characters/'.$hCharacter['shortName'].'/'.$hCharacter['characterID'].'/">'.$hCharacter['label'].'</a></li>'."\n";
+					$count++;
 				}
-				echo "					".'<li><a href="/characters/'.$hCharacter['shortName'].'/'.$hCharacter['characterID'].'/">'.$hCharacter['label'].'</a></li>'."\n";
-				$count++;
+				echo "				</ul>\n";
 			}
-			echo "				</ul>\n";
-		}
-?>
+	?>
 			</li>
 			<li>
 				<a href="/games/">Games</a>
 <?
-		$header_games = $mysql->query('SELECT g.gameID, g.title, p.isGM FROM games g INNER JOIN players p ON p.userID = '.intval($_SESSION['userID']).' AND p.gameID = g.gameID ORDER BY g.title');
-		if ($header_games->rowCount()) {
-			echo "				<ul>\n";
-			$count = 0;
-			foreach ($header_games as $game) {
-				echo "					".'<li><a href="/games/'.$game['gameID'].'/">'.$game['title'].($game['isGM']?' <img src="/images/gm_icon.png">':'').'</a></li>'."\n";
-				$count++;
-				if ($count == 5) {
-					echo "					".'<li><a href="/games/my/">All games</a></li>'."\n";
-					break;
+			$header_games = $mysql->query('SELECT g.gameID, g.title, p.isGM FROM games g INNER JOIN players p ON p.userID = '.intval($_SESSION['userID']).' AND p.gameID = g.gameID ORDER BY g.title');
+			if ($header_games->rowCount()) {
+				echo "				<ul>\n";
+				$count = 0;
+				foreach ($header_games as $game) {
+					echo "					".'<li><a href="/games/'.$game['gameID'].'/">'.$game['title'].($game['isGM']?' <img src="/images/gm_icon.png">':'').'</a></li>'."\n";
+					$count++;
+					if ($count == 5) {
+						echo "					".'<li><a href="/games/my/">All games</a></li>'."\n";
+						break;
+					}
 				}
+				echo "				</ul>\n";
 			}
-			echo "				</ul>\n";
-		}
 ?>
 			</li>
-<? } ?>
+<?		} ?>
 			<li><a href="/forums/">Forums</a></li>
-<? if ($loggedIn) { ?>
+<?		if ($loggedIn) { ?>
 			<li><a href="/gamersList/">The Gamers</a></li>
-<? } ?>
+<?		} ?>
 		</ul>
 		<ul id="mainMenu_right">
 			<li><a href="/faqs/" class="first">FAQs</a></li>
@@ -97,6 +97,6 @@
 			<p>Hm... seems like you're using IE. Can I suggest a better browser, such as <a href="http://www.mozilla.com/en-US/firefox/" target="_blank">Firefox</a>, <a href="http://www.googlechrome.com/" target="_blank">Chrome</a> or <a href="http://www.opera.com/" target="_blank">Opera</a>? There are other choices too.</p>
 			<p>If you wanna stick with IE, or can't switch, I'll warn you right now, while most of this site should work with IE, stuff might come up buggy, so you might not enjoy it as much...</p>
 		</div>
-<? } else { ?>
+<?	} else { ?>
 <div id="page_<?=PAGE_ID?>" class="clearfix<?=sizeof($bodyClasses)?' '.implode(' ', $bodyClasses):''?>">
-<? } ?>
+<?	} ?>
