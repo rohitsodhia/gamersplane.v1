4a465f22e1ceaf3d1797d0c45d239056439bc5ce
Rohit Sodhia <rohit@rhovisions.com>
2014-12-17 08:50:00 -0500

    Add index on heritage, start forum class

---
+++ includes/forums/Forum.class.php
@@ -0,0 +1,15 @@
+<?
+	class Forum {
+		protected $forumID ;
+		protected $title;
+		protected $description;
+		protected $forumType;
+		protected $parentID;
+		protected $heritage;
+		protected $order;
+		protected $gameID;
+		
+		public function __construct() {
+		}
+	}
+?>
