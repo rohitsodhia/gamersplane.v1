63bcf1131b19b1892275105422bf921aaf3551aa
Rohit Sodhia <rohit@rhovisions.com>
2015-01-09 14:54:23 -0500

    Update users

--- ucp/user.php
+++ ucp/user.php
@@ -1,17 +1,16 @@
 <?
 	$profileID = intval($pathOptions[0]);
-	$userCheck = $mysql->query('SELECT * FROM users WHERE userID = '.$profileID);
-	if ($userCheck->rowCount() == 0) { header('Location: /404'); exit; }
-	$userInfo = $userCheck->fetch();
-	
+	$user = new User($profileID);
+ 	if ($user->userID == 0) { header('Location: /404'); exit; }
+	$user->getAllUsermeta();
 	$profFields = array('location' => 'Location', 'aim' => 'AIM', 'yahoo' => 'Yahoo!', 'msn' => 'MSN', 'games' => 'Games');
 ?>
 <? require_once(FILEROOT.'/header.php'); ?>
-		<h1 class="headerbar"><?=$userInfo['username']?></h1>
+		<h1 class="headerbar"><?=$user->username?></h1>
 		<div id="leftCol">
-			<img src="<?='/ucp/avatars/'.(file_exists(FILEROOT."/ucp/avatars/{$postInfo['userID']}.png")?$postInfo['userID']:'avatar')?>.png" class="avatar">
+			<img src="<?=$user->getAvatar()?>" class="avatar">
 			<div id="actions">
-				<a href="/pms/send/?userID=<?=$profileID?>">Send Private Message</a>
+				<a href="/pms/send/?userID=<?=$user->userID?>">Send Private Message</a>
 			</div>
 		</div>
 		<div id="rightCol">
@@ -20,23 +19,23 @@
 				<div class="details">
 					<div class="tr">
 						<div class="title">Member Since</div>
-						<div class="convertTZ"><?=date('F j, Y g:i A', strtotime($userInfo['joinDate']))?></div>
+						<div class="convertTZ"><?=date('F j, Y g:i A', strtotime($user->joinDate))?></div>
 					</div>
-<? if ($userInfo['gender'] != '') { ?>
+<? if ($user->gender != '') { ?>
 					
 					<div class="tr">
 						<div class="title">Gender</div>
-						<div><?=$userInfo['gender'] == 'm'?'Male':'Female'?></div>
+						<div><?=$user->gender == 'm'?'Male':'Female'?></div>
 					</div>
 <?
 	}
-	if ($userInfo['showAge'] == 1) {
-		$thisYear = strtotime(date('Y').substr($userInfo['birthday'], 4));
+	if ($user->showAge == 1) {
+		$thisYear = strtotime(date('Y').substr($user->birthday, 4));
 ?>
 					
 					<div class="tr">
 						<div class="title">Age</div>
-						<div><?=date('Y') - substr($userInfo['birthday'], 0, 4) - ($thisYear > time()?1:0)?></div>
+						<div><?=date('Y') - substr($user->birthday, 0, 4) - ($thisYear > time()?1:0)?></div>
 					</div>
 <? } ?>
 <? foreach ($profFields as $field => $label) { if (strlen($userInfo[$field])) { ?>
@@ -62,7 +61,7 @@
 			$numChars += $info['numChars'];
 		}
 		$count = 0;
-		echo "\t\t\t\t\t<p>{$userInfo['username']} has made $numChars character".($numChars == 1?'':'s')." so far.</p>\n";
+		echo "\t\t\t\t\t<p>{$user->username} has made $numChars character".($numChars == 1?'':'s')." so far.</p>\n";
 		foreach ($charStats as $game) {
 			$count++;
 			echo "\t\t\t\t\t<div class=\"game".($count % 3 == 0?' third':'')."\">\n";
@@ -73,7 +72,7 @@
 			echo "\t\t\t\t\t\t</div>\n";
 			echo "\t\t\t\t\t</div>\n";
 		}
-	} else echo "\t\t\t\t<div>{$userInfo['username']} has not yet made any characters.</div>\n";
+	} else echo "\t\t\t\t<div>{$user->username} has not yet made any characters.</div>\n";
 ?>
 				</div>
 			</div>
@@ -91,7 +90,7 @@
 			$numGames += $info['numGames'];
 		}
 		$count = 0;
-		echo "\t\t\t\t\t<p>{$userInfo['username']} has run $numGames game".($numGames == 1?'':'s')." so far.</p>\n";
+		echo "\t\t\t\t\t<p>{$user->username} has run $numGames game".($numGames == 1?'':'s')." so far.</p>\n";
 		foreach ($gameStats as $game) {
 			$count++;
 			echo "\t\t\t\t\t<div class=\"game".($count % 3 == 0?' third':'')."\">\n";
@@ -102,7 +101,7 @@
 			echo "\t\t\t\t\t\t</div>\n";
 			echo "\t\t\t\t\t</div>\n";
 		}
-	} else echo "\t\t\t\t<div>{$userInfo['username']} has not yet run any games.</div>\n";
+	} else echo "\t\t\t\t<div>{$user->username} has not yet run any games.</div>\n";
 ?>
 				</div>
 			</div>
