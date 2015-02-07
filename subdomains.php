4b3f259459eab133cb6ddb5df8edaf83ac952a8a
Rohit Sodhia <rohit@rhovisions.com>
2015-02-07 04:26:33 -0500

    Forgot to explode correctly

--- subdomains.php
+++ subdomains.php
@@ -1,8 +1,9 @@
 <?
-	$subdomainParts = explode($__SERVER['HTTP_HOST']);
+	$subdomainParts = explode('.', $_SERVER['HTTP_HOST']);
 	$subdomain = $subdomainParts[0];
+	$redirect = '';
 	if ($subdomain == 'www') $redirect = 'http://gamersplane.com';
 	elseif ($subdomain == 'amazon') $redirect = 'http://smile.amazon.com/?_encoding=UTF8&camp=1789&creative=9325&linkCode=ur2&tag=gampla0e6-20&linkId=7RQR4I66XH6Z2U4B';
 	elseif ($subdomain == 'dtrpg') $redirect = 'http://rpg.drivethrustuff.com/browse.php?affiliate_id=739399';
-	header('Location: '.$redirect);
+	if ($redirect != '') header('Location: '.$redirect);
 ?>
