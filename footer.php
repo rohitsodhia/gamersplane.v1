5bc01c87771fd6ffe084f0716034f9426a774390
Rohit Sodhia <rohit@rhovisions.com>
2015-02-08 09:27:24 -0500

    Need p tags around footer links

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
 	<a href="/contact/">Contact Us</a>
 <?		if ($currentUser->checkACP()) { ?>
 	<div class="floatRight">
-		<a href="http://amazon.gamersplane.com" target="_blank">Amazon referral link</a>
-		<a href="http://dtrp.gamersplane.com" target="_blank">DTRPG referral link</a>
+		<p><a href="http://amazon.gamersplane.com" target="_blank">Amazon referral link</a></p>
+		<p><a href="http://dtrp.gamersplane.com" target="_blank">DTRPG referral link</a></p>
 		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="6VHQ2BP4AS7L6">
