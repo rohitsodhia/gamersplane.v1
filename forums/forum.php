c61f4789ebc96ca5a39517a75f6d45d2bfe0f93d
Rohit Sodhia <rohit@rhovisions.com>
2015-02-20 01:48:30 -0500

    Default page

--- forums/forum.php
+++ forums/forum.php
@@ -47,7 +47,7 @@
 	if ($forumID && $forumManager->getForumProperty($forumID, 'forumType') == 'f') {
 		$forumManager->getThreads();
 		$forumManager->displayThreads();
-		ForumView::displayPagination($forumManager->getForumProperty('threadCount'), $_GET['page']);
+		ForumView::displayPagination($forumManager->getForumProperty('threadCount'), isset($_GET['page'])?$_GET['page']:1);
 	}
 ?>
 <?
