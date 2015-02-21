106b55c8cba811496853d6b58844da305104ec3c
Rohit Sodhia <rohit@rhovisions.com>
2015-02-20 19:00:23 -0500

    Adjust pagination

--- includes/forums/ForumView.class.php
+++ includes/forums/ForumView.class.php
@@ -3,7 +3,7 @@
 		public function __construct() {
 		}
 
-		public static function displayPagination($count = 0, $page = 1) {
+		public static function displayPagination($count = 0, $page = 1, $query = array()) {
 			$page = intval($page) > 0?intval($page):1;
 			if ($page > ceil($count / PAGINATE_PER_PAGE)) $page = ceil($count / PAGINATE_PER_PAGE);
 
@@ -16,12 +16,12 @@
 				$lastPage = $page + $spread;
 				if ($lastPage > $numPages) $lastPage = $numPages;
 				echo "\t\t\t\t<div class=\"currentPage\">{$page} of {$numPages}</div>\n";
-				if (($page - $spread) > 1) echo "\t\t\t\t<a href=\"?page=1\">&lt;&lt; First</a>\n";
-				if ($page > 1) echo "\t\t\t\t<a href=\"?page=".($page - 1)."\">&lt;</a>\n";
-				for ($count = $firstPage; $count <= $lastPage; $count++) echo "\t\t\t\t<a href=\"?page=$count\"".(($count == $page)?' class="page"':'').">$count</a>\n";
+				if (($page - $spread) > 1) echo "\t\t\t\t<a href=\"?".http_build_query(array_merge($query, array('page' => 1)))."\">&lt;&lt; First</a>\n";
+				if ($page > 1) echo "\t\t\t\t<a href=\"?".http_build_query(array_merge($query, array('page' => $page - 1)))."\">&lt;</a>\n";
+				for ($count = $firstPage; $count <= $lastPage; $count++) echo "\t\t\t\t<a href=\"?".http_build_query(array_merge($query, array('page' => $count)))."\"".(($count == $page)?' class="page"':'').">$count</a>\n";
 				
-				if ($page < $numPages) echo "\t\t\t\t<a href=\"?page=".($page + 1)."\">&gt;</a>\n";
-				if (($page + $spread) < $numPages) echo "\t\t\t\t<a href=\"?page={$numPages}\">Last &gt;&gt;</a>\n";
+				if ($page < $numPages) echo "\t\t\t\t<a href=\"?".http_build_query(array_merge($query, array('page' => $page + 1)))."\">&gt;</a>\n";
+				if (($page + $spread) < $numPages) echo "\t\t\t\t<a href=\"?".http_build_query(array_merge($query, array('page' => $numPages)))."\">Last &gt;&gt;</a>\n";
 				echo "\t\t\t</div>\n";
 				echo "\t\t\t<br class=\"clear\">\n";
 			}
