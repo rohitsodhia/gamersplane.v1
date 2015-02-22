b3c3203ee4e252dd932b49ecb3f34595e870898d
Rohit Sodhia <rohit@rhovisions.com>
2015-02-22 02:20:09 -0500

    Another search display correction

--- includes/forums/ForumSearch.class.php
+++ includes/forums/ForumSearch.class.php
@@ -95,7 +95,7 @@
 						</div>
 <?				} ?>
 						<a href="/forums/thread/<?=$result->threadID?>/"><?=$result->title?></a><br>
-						<span class="threadAuthor">by <a href="/user/<?=$result->authorID?>/" class="username"><?=$result->username?></a> in <a href="/forums/<?=$result->forumID?>/"><?=$result->fTitle?></a> on <span class="convertTZ"><?=date('M j, Y g:i a', strtotime($result->datePosted))?></span></span>
+						<span class="threadAuthor">by <a href="/user/<?=$result->authorID?>/" class="username"><?=$result->username?></a> in <a href="/forums/<?=$result->forumID?>/"><?=$result->forum?></a> on <span class="convertTZ"><?=date('M j, Y g:i a', strtotime($result->datePosted))?></span></span>
 					</div>
 					<div class="td numPosts"><?=$result->postCount?></div>
 					<div class="td lastPost">
