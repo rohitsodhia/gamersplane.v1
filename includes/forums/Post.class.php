0b205f89f061c9f730a838ec2ffa207d41d8967b
Rohit Sodhia <rohit@rhovisions.com>
2015-02-24 16:38:31 -0500

    Fix adding new rolls to edit

--- includes/forums/Post.class.php
+++ includes/forums/Post.class.php
@@ -130,39 +130,26 @@
 				$addPost->bindValue(':datePosted', date('Y-m-d H:i:s'));
 				$addPost->execute();
 				$this->postID = $mysql->lastInsertId();
-
-				foreach ($this->rolls as $roll) 
-					$roll->forumSave($this->postID);
-
-				if (sizeof($this->draws)) {
-					$addDraw = $mysql->prepare("INSERT INTO deckDraws SET postID = {$this->postID}, deckID = :deckID, type = :type, cardsDrawn = :cardsDrawn, reveals = :reveals, reason = :reason");
-					foreach($this->draws as $deckID => $draw) {
-						$mysql->query("UPDATE decks SET position = position + {$draw['draw']} WHERE deckID = {$deckID}");
-						$addDraw->bindValue('deckID', $deckID);
-						$addDraw->bindValue('type', $draw['type']);
-						$addDraw->bindValue('cardsDrawn', $draw['cardsDrawn']);
-						$addDraw->bindValue('reveals', str_repeat('0', $draw['draw']));
-						$addDraw->bindValue('reason', $draw['reason']);
-						$addDraw->execute();
-					}
-				}
 			} else {
 				$updatePost = $mysql->prepare("UPDATE posts SET title = :title, message = :message, postAs = ".($this->postAs?$this->postAs:'NULL').($this->edited?", lastEdit = NOW(), timesEdited = {$this->timesEdited}":'')." WHERE postID = {$this->postID}");
 				$updatePost->bindValue(':title', $this->title);
 				$updatePost->bindValue(':message', $this->message);
 				$updatePost->execute();
+			}
 
-				if (sizeof($this->draws)) {
-					$addDraw = $mysql->prepare("INSERT INTO deckDraws SET postID = {$this->postID}, deckID = :deckID, type = :type, cardsDrawn = :cardsDrawn, reveals = :reveals, reason = :reason");
-					foreach($this->draws as $deckID => $draw) {
-						$mysql->query("UPDATE decks SET position = position + {$draw['draw']} WHERE deckID = {$deckID}");
-						$addDraw->bindValue('deckID', $deckID);
-						$addDraw->bindValue('type', $draw['type']);
-						$addDraw->bindValue('cardsDrawn', $draw['cardsDrawn']);
-						$addDraw->bindValue('reveals', str_repeat('0', $draw['draw']));
-						$addDraw->bindValue('reason', $draw['reason']);
-						$addDraw->execute();
-					}
+			foreach ($this->rolls as $roll) 
+				$roll->forumSave($this->postID);
+
+			if (sizeof($this->draws)) {
+				$addDraw = $mysql->prepare("INSERT INTO deckDraws SET postID = {$this->postID}, deckID = :deckID, type = :type, cardsDrawn = :cardsDrawn, reveals = :reveals, reason = :reason");
+				foreach($this->draws as $deckID => $draw) {
+					$mysql->query("UPDATE decks SET position = position + {$draw['draw']} WHERE deckID = {$deckID}");
+					$addDraw->bindValue('deckID', $deckID);
+					$addDraw->bindValue('type', $draw['type']);
+					$addDraw->bindValue('cardsDrawn', $draw['cardsDrawn']);
+					$addDraw->bindValue('reveals', str_repeat('0', $draw['draw']));
+					$addDraw->bindValue('reason', $draw['reason']);
+					$addDraw->execute();
 				}
 			}
 
