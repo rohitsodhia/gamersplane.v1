1f7077e0f8b29af6f4a550251652389cde822b13
Rohit Sodhia <rohit@rhovisions.com>
2015-02-25 00:38:42 -0500

    No cacheing

--- meta.php
+++ meta.php
@@ -7,3 +7,9 @@
 	<meta property="og:type" content="website">
 	<meta property="og:description" content="Gamers' Plane is a pen and paper RPG play-by-post site and community. Including tools like integrated dice, character library, and more, Gamers' Plane is a great place to get into a game online, or organize your live ones.">
 	<meta property="og:image" content="http://gamersplane.com/images/logo.jpg">
+
+	<meta http-equiv="cache-control" content="max-age=0">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
+	<meta http-equiv="pragma" content="no-cache">
