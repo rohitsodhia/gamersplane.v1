dab685fada0661fd239eedab02079264a57abf2c
Rohit Sodhia <rohit@rhovisions.com>
2015-02-24 23:31:23 -0500

    Enforce lightbox height

--- characters/avatar.php
+++ characters/avatar.php
@@ -16,7 +16,7 @@
 ?>
 		<h1 class="headerbar">Character Avatar</h1>
 
-		<form method="post" action="/characters/process/avatar/" enctype="multipart/form-data" class="hbMargined" class="height: 257px;">
+		<form method="post" action="/characters/process/avatar/" enctype="multipart/form-data" class="hbMargined" style="height: 257px;">
 <?	if (!$charPermissions) { ?>
 			<p>Seems like you're trying to change a character that isn't yours!</p>
 <?	} ?>
