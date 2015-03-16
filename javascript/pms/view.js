31c83a760c87059ec297f9aad17e756dfd2b70fc
Rohit Sodhia <rohit@rhovisions.com>
2015-03-15 23:08:12 -0400

    Try SCE

--- javascript/pms/view.js
+++ javascript/pms/view.js
@@ -1,4 +1,4 @@
-controllers.controller('pmView', function ($scope, $cookies, $http, $compile, DeletePM) {
+controllers.controller('pmView', function ($scope, $cookies, $http, $sce, DeletePM) {
 	pathElements = getPathElements();
 
 	$scope.allowDelete = true;
@@ -6,7 +6,6 @@ controllers.controller('pmView', function ($scope, $cookies, $http, $compile, De
 		if (data.failed || data.noPM) 
 			document.location = '/pms/';
 
-		data.message = $compile(data.message);
 		data.datestamp = convertTZ(data.datestamp, 'YYYY-MM-DD HH:mm:ss', 'MMMM D, YYYY h:mm a');
 		for (key in data) 
 			$scope[key] = data[key];
