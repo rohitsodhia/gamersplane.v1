1a6dcc34839870315729efd6deb4517efdc8922d
Rohit Sodhia <rohit@rhovisions.com>
2015-04-04 01:57:40 -0400

    Setup grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,40 @@
 module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+			dev: {
+				files: [{
+					expand: true,
+					cwd: 'styles/',
+					src: ['**/*.less'],
+					dest: 'styles/',
+					ext: '.css'
+				}]
+			},
+			prod: {
+				options: {
+					compress: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'styles/',
+					src: ['**/*.less'],
+					dest: 'styles/',
+					ext: '.css'
+				}]
+			}
+		},
+		watch: {
+			less: {
+				files: ['styles/**/*.less'],
+				tasks: ['less:dev']
+			}
+		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['less:dev']);
 };
