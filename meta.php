363862dbb7cf23a9d38d726e5596521ccc706098
Rohit Sodhia <rohit@rhovisions.com>
2015-04-08 00:23:07 -0400

    Set charset to uft-8

--- meta.php
+++ meta.php
@@ -1,4 +1,4 @@
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Gamers Plane<?=strlen($dispatchInfo['title'])?' - '.$dispatchInfo['title']:''?></title>
 
 	<link href="/images/favicon.ico" rel="shortcut icon">
