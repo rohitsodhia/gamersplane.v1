b9accf88b52cbab6fa5259309b3ae8ee2230a03d
Rohit Sodhia <rohit@rhovisions.com>
2015-04-08 17:22:12 -0400

    Fix Shadowrun bugs

--- characters/shadowrun4/sheet.php
+++ characters/shadowrun4/sheet.php
@@ -1,10 +1,10 @@
 			<div class="tr clearfix">
 				<label for="name">Name:</label>
-				<div><?=$this->getName()?></div>
+				<div><?=printReady($this->getName()?></div>
 			</div>
 			<div class="tr clearfix">
 				<label for="metatype">Metatype:</label>
-				<div><?=$this->getMetatype()?></div>
+				<div><?=printReady($this->getMetatype()?></div>
 			</div>
 			
 			<div class="clearfix">
@@ -27,7 +27,7 @@
 				
 				<div id="qualities">
 					<h2 class="headerbar hbDark">Qualities</h2>
-					<div class="hbdMargined"><?=$this->getQualities()?></div>
+					<div class="hbdMargined"><?=printReady($this->getQualities()?></div>
 				</div>
 				
 				<div id="damage">
@@ -48,42 +48,42 @@
 			<div class="clearfix">
 				<div id="skills" class="twoCol floatLeft">
 					<h2 class="headerbar hbDark">Skills</h2>
-					<div class="hbdMargined"><?=$this->getSkills()?></div>
+					<div class="hbdMargined"><?=printReady($this->getSkills()?></div>
 				</div>
 				<div id="spells" class="twoCol floatRight">
 					<h2 class="headerbar hbDark">Spells</h2>
-					<div class="hbdMargined"><?=$this->getSpells()?></div>
+					<div class="hbdMargined"><?=printReady($this->getSpells()?></div>
 				</div>
 			</div>
 			
 			<div class="clearfix">
 				<div id="weapons" class="twoCol floatLeft">
 					<h2 class="headerbar hbDark">Weapons</h2>
-					<div class="hbdMargined"><?=$this->getWeapons()?></div>
+					<div class="hbdMargined"><?=printReady($this->getWeapons()?></div>
 				</div>
 				<div id="armor" class="twoCol floatRight">
 					<h2 class="headerbar hbDark">Armor</h2>
-					<div class="hbdMargined"><?=$this->getArmor()?></div>
+					<div class="hbdMargined"><?=printReady($this->getArmor()?></div>
 				</div>
 			</div>
 			
 			<div class="clearfix">
 				<div id="augments" class="twoCol floatLeft">
 					<h2 class="headerbar hbDark">Augments</h2>
-					<div class="hbdMargined"><?=$this->getAugments()?></div>
+					<div class="hbdMargined"><?=printReady($this->getAugments()?></div>
 				</div>
 				<div id="contacts" class="twoCol floatRight">
 					<h2 class="headerbar hbDark">Contacts</h2>
-					<div class="hbdMargined"><?=$this->getContacts()?></div>
+					<div class="hbdMargined"><?=printReady($this->getContacts()?></div>
 				</div>
 			</div>
 			
 			<div id="items">
 				<h2 class="headerbar hbDark">Items</h2>
-				<div class="hbdMargined"><?=$this->getItems()?></div>
+				<div class="hbdMargined"><?=printReady($this->getItems())?></div>
 			</div>
 			
 			<div id="notes">
 				<h2 class="headerbar hbDark">Notes</h2>
-				<div class="hbdMargined"><?=$this->getNotes()?></div>
+				<div class="hbdMargined"><?=printReady($this->getNotes())?></div>
 			</div>
