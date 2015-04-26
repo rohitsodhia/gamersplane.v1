78de8db3c03b6fa2b1336cee5d798484eb3ea98e
Rohit Sodhia <rohit@rhovisions.com>
2015-04-25 22:30:07 -0400

    Removing old code

--- javascript/forums/forum.js
+++ javascript/forums/forum.js
@@ -1,7 +1,4 @@
 $(function () {
-	leftSpacing = parseInt($('.hbDark .dlWing').css('borderRightWidth').slice(0, -2));
-	$('h2, #newThread > a').css('marginLeft', leftSpacing + 'px');
-
 	$('#forumSub').click(function (e) {
 		e.preventDefault();
 
