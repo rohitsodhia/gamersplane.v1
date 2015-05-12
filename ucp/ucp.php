84805ad1703332b04e18aa12d756fc0a2ad08085
Rohit Sodhia <rohit@rhovisions.com>
2015-05-11 23:52:10 -0400

    Duplciated field

--- ucp/ucp.php
+++ ucp/ucp.php
@@ -93,10 +93,6 @@
 					<div><input id="games" type="text" name="games" value="<?=printReady($user->games)?>"></div>
 				</div>
 				<div class="tr">
-					<label>Recieve new game emails?</label>
-					<div><input type="radio" name="newGameMail" value="1"<?=$user->newGameMail == 1?' checked="checked"':''?>> Yes <input type="radio" name="newGameMail" value="0"<?=$user->newGameMail == 0?' checked="checked"':''?>> No</div>
-				</div>
-				<div class="tr">
 					<label>Recieve PM emails?</label>
 					<div><input type="radio" name="pmMail" value="1"<?=$user->pmMail == 1?' checked="checked"':''?>> Yes <input type="radio" name="pmMail" value="0"<?=$user->pmMail == 0?' checked="checked"':''?>> No</div>
 				</div>
