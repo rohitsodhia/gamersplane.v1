d450558d9a312d51398d9df22a34416b44d616a4
Rohit Sodhia <rohit@rhovisions.com>
2015-06-17 14:39:16 -0400

    Adjust reset code slightly

--- login/process/requestReset.php
+++ login/process/requestReset.php
@@ -7,6 +7,7 @@
 		if ($userCheck->rowCount()) {
 			$user = new User($email);
 			$validationStr = substr(md5($email.substr($user->password, 3).$user->activatedOn), 4, 13);
+			echo "http://gamersplane.com/login/resetPass/?email={$email}&validate={$validationStr}"; exit;
 			$body = "Gamers Plane Password Reset
 
 ---------------------------------------------------
@@ -15,13 +16,11 @@ To reset your password, please go to
 
 http://gamersplane.com/login/resetPass/?email={$email}&validate={$validationStr}
 
-Enter your current username and the following code
-
-{$rndPass}
-
 It will take you to a page where you can enter a new password of your choice.";
 			mail($email, 'Gamers Plane Password Reset', $body, "From: contact@gamersplane.com\r\nReply-To: contact@gamersplane.com");
 			header('Location: /login/requestReset/?success=1'.(isset($_POST['modal'])?'&modal=1':''));
-		} else header('Location: /login/requestReset/?invalidEmail=1'.(isset($_POST['modal'])?'&modal=1':''));
-	} else header('Location: /login/requestReset/'.(isset($_POST['modal'])?'?modal=1':''));
+		} else 
+			header('Location: /login/requestReset/?invalidEmail=1'.(isset($_POST['modal'])?'&modal=1':''));
+	} else 
+		header('Location: /login/requestReset/'.(isset($_POST['modal'])?'?modal=1':''));
 ?>
