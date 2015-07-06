f7e0e42564c02281a0604ec669d5fc1e3ea266d1
Rohit Sodhia <rohit@rhovisions.com>
2015-07-05 23:45:37 -0400

    Temp switch

--- acp/music.php
+++ acp/music.php
@@ -14,23 +14,21 @@
 			<a id="addMusic" href="/tools/music/add/" class="fancyButton smallButton">Add Music</a>
 			<music-form data="newSong"></music-form>
 			<ul class="prettyList">
-				<li ng-repeat="song in music" ng-class="{ 'unapproved': !song.approved }">
-					<div class="songDetails">
-						<div class="clearfix">
-							<a href="{{song.url}}" target="_blank" class="song">{{song.title}}<img src="/images/tools/quote.png" ng-if="song.lyrics" title="Has Lyrics" alt="Has Lyrics"></a>
-							<div class="manageSong">
-								<a ng-click="toggleApproval(song)" href="" class="toggleApproval">{{song.approved?'Una':'A'}}pprove</a>
-								<a href="" class="delete">Delete</a>
-								<span class="confirmDelete">(
-									<a href="" class="confirm">Confirm</a>
-									<a href="" class="deny">Deny</a>
-								)</span>
-								<a href="" class="edit">Edit</a>
-							</div>
+				<li ng-repeat="song in music" class="songDetails" ng-class="{ 'unapproved': !song.approved }">
+					<div class="clearfix">
+						<a href="{{song.url}}" target="_blank" class="song">{{song.title}}<img src="/images/tools/quote.png" ng-if="song.lyrics" title="Has Lyrics" alt="Has Lyrics"></a>
+						<div class="manageSong">
+							<a ng-click="toggleApproval(song)" href="" class="toggleApproval">{{song.approved?'Una':'A'}}pprove</a>
+							<a href="" ng-click="editSong(song)">Edit</a>
+							<a href="" class="delete">Delete</a>
+							<span class="confirmDelete">(
+								<a href="" class="confirm">Confirm</a>
+								<a href="" class="deny">Deny</a>
+							)</span>
 						</div>
-						<div class="genres">{{song.genres.join(', ')}}</div>
-						<div ng-if="song.notes.length" class="notes">{{song.notes}}</div>
 					</div>
+					<div class="genres">{{song.genres.join(', ')}}</div>
+					<div ng-if="song.notes.length" class="notes">{{song.notes}}</div>
 				</li>
 			</ul>
 			<paginate class="tr"></paginate>
