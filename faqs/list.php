c863b6e0fb3d8bccd8e3238e6fe6ed1fbbd9c021
Rohit Sodhia <rohit@rhovisions.com>
2015-09-20 18:58:54 -0400

    Fix sort order

--- faqs/list.php
+++ faqs/list.php
@@ -4,11 +4,11 @@
 			<li ng-repeat="(slug, category) in catMap"><a href="#{{slug}}">{{category}}</a></li>
 		</ul></div>
 		<div class="mainColumn right">
-			<div ng-repeat="(category, faqs) in aFAQs">
-				<a name="{{category}}"></a>
-				<h2 class="headerbar hbDark" skew-element>{{catMap[category]}}</h2>
+			<div ng-repeat="(slug, category) in catMap" ng-if="aFAQs[slug].length">
+				<a name="{{slug}}"></a>
+				<h2 class="headerbar hbDark" skew-element>{{category}}</h2>
 				<div class="faqs" hb-margined>
-					<div ng-repeat="faq in faqs | orderBy: 'order'" class="faq">
+					<div ng-repeat="faq in aFAQs[slug] | orderBy: 'order'" class="faq">
 						<div class="question">{{faq.question}}</div>
 						<div class="answer" ng-bind-html="faq.answer.encoded | trustHTML"></div>
 					</div>
