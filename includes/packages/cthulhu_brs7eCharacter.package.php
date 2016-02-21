87d61b2a427178a656d04183ee5e6dd2be2681ec
Rohit Sodhia <rohit@rhovisions.com>
2016-02-20 23:00:21 -0500

    Fix CoC package

--- includes/packages/cthulhu_brs7eCharacter.package.php
+++ includes/packages/cthulhu_brs7eCharacter.package.php
@@ -1,4 +1,4 @@
 <?
 	require_once(FILEROOT.'/includes/packages/Character.package.php');
-	require_once(FILEROOT.'/includes/characters/cthulhud100_7e/cthulhu_brs7eCharacter.class.php');
+	require_once(FILEROOT.'/includes/characters/cthulhu_brs7e/cthulhu_brs7eCharacter.class.php');
 ?>
