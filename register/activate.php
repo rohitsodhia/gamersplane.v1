5a4628adf1eeb6c4898b831619c5ef7167af30d6
Rohit Sodhia <rohit@rhovisions.com>
2016-07-24 21:57:38 -0400

    Add new player guide to activation link

--- register/activate.php
+++ register/activate.php
@@ -30,7 +30,7 @@
 <?	if (isset($currentUser) && $currentUser != null) { ?>
 		<h1 class="headerbar">Account Activated!</h1>
 		<p>Congratulations, <b><?=$currentUser->username?></b>! Your account has been activiated.</p>
-		<p>We recommend you check out the <a href="/faqs/">FAQs</a> to get an idea of what you can do to get started. You can also head straight to make a new <a href="/characters/my/">character</a> or find a <a href="/games/list/">game</a>, and be sure to stop by the <a href="/forums/">forums</a> and <a href="/forums/14/">introduce yourself</a>!</p>
+		<p>We recommend you check out the <a href="/faqs/">FAQs</a> and our <a href="http://gamersplane.com/forums/thread/2461/">New Player Guide</a> to get an idea of what you can do to get started. You can also head straight to make a new <a href="/characters/my/">character</a> or find a <a href="/games/list/">game</a>, and be sure to stop by the <a href="/forums/">forums</a> and <a href="/forums/14/">introduce yourself</a>!</p>
 <?	} else { ?>
 		<h1 class="headerbar">Sorry...</h1>
 		<p>Sorry, but the account you are trying to activate has already been activated or does not exist.</p>
