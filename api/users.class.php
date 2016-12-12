a763c2c84a1a4ae1b45e91f808ee53f86b7749ef
Rohit Sodhia <rohit@rhovisions.com>
2016-12-11 22:26:01 -0500

    UCP fix

--- api/users.class.php
+++ api/users.class.php
@@ -239,34 +239,45 @@
 		public function saveUser() {
 			global $loggedIn, $currentUser;
 
-			if (!$loggedIn && $currentUser->userID != $_POST['userID'] && !$currentUser->checkACP('users', false))
-				displayJSON(array('failed' => true, 'noAuth' => true));
+			if (!$loggedIn && $currentUser->userID != $_POST['userID'] && !$currentUser->checkACP('users', false)) {
+				displayJSON([
+					'failed' => true,
+					'noAuth' => true
+				]);
+			}
 
-			$data = json_decode($_POST['data']);
-			$details = $data->details;
-			$newPass = $data->newPass;
-			$userID = (int) $details->userID;
-			if ($currentUser->userID != $userID)
+			$details = $_POST['details'];
+			var_dump($details); exit;
+			$newPass = $_POST['newPass'];
+			$userID = (int) $details['userID'];
+			if ($currentUser['userID'] != $userID) {
 				$user = new User($userID);
-			else
+			} else {
 				$user = $currentUser;
+			}
 
 			$avatarUploaded = false;
-			if ($data->avatar->delete)
+			if ($details['avatar']['delete']) {
 				unlink(FILEROOT."/ucp/avatars/{$user->userID}.jpg");
+			}
 			if ($_FILES['file']['error'] == 0 && $_FILES['file']['size'] > 15 && $_FILES['file']['size'] < 1048576) {
 				$avatarExt = trim(end(explode('.', strtolower($_FILES['file']['name']))));
-				if ($avatarExt == 'jpeg')
+				if ($avatarExt == 'jpeg') {
 					$avatarExt = 'jpg';
+				}
 				if (in_array($avatarExt, array('jpg', 'gif', 'png'))) {
 					$maxWidth = 150;
 					$maxHeight = 150;
 
 					list($imgWidth, $imgHeight, $imgType) = getimagesize($_FILES['file']['tmp_name']);
 					if ($imgWidth >= $maxWidth && $imgHeight >= $maxHeight) {
-						if (image_type_to_mime_type($imgType) == 'image/jpeg' || image_type_to_mime_type($imgType) == 'image/pjpeg') $tempImg = imagecreatefromjpeg($_FILES['file']['tmp_name']);
-						elseif (image_type_to_mime_type($imgType) == 'image/gif') $tempImg = imagecreatefromgif($_FILES['file']['tmp_name']);
-						elseif (image_type_to_mime_type($imgType) == 'image/png') $tempImg = imagecreatefrompng($_FILES['file']['tmp_name']);
+						if (image_type_to_mime_type($imgType) == 'image/jpeg' || image_type_to_mime_type($imgType) == 'image/pjpeg') {
+							$tempImg = imagecreatefromjpeg($_FILES['file']['tmp_name']);
+						} elseif (image_type_to_mime_type($imgType) == 'image/gif') {
+							$tempImg = imagecreatefromgif($_FILES['file']['tmp_name']);
+						} elseif (image_type_to_mime_type($imgType) == 'image/png') {
+							$tempImg = imagecreatefrompng($_FILES['file']['tmp_name']);
+						}
 
 						$xRatio = $maxWidth / $imgWidth;
 						$yRatio = $maxHeight / $imgHeight;
@@ -288,80 +299,94 @@
 						imagecopyresampled($tempColor, $tempImg, 0, 0, 0, 0, $finalWidth, $finalHeight, $imgWidth, $imgHeight);
 
 						$destination = FILEROOT.'/ucp/avatars/'.$user->userID.'.'.$avatarExt;
-						foreach (glob(FILEROOT.'/ucp/avatars/'.$user->userID.'.*') as $oldFile)
+						foreach (glob(FILEROOT.'/ucp/avatars/'.$user->userID.'.*') as $oldFile) {
 							unlink($oldFile);
-						if ($avatarExt == 'jpg')
+						}
+						if ($avatarExt == 'jpg') {
 							imagejpeg($tempColor, $destination, 100);
-						elseif ($avatarExt == 'gif')
+						} elseif ($avatarExt == 'gif') {
 							imagegif($tempColor, $destination);
-						elseif ($avatarExt == 'png')
+						} elseif ($avatarExt == 'png') {
 							imagepng($tempColor, $destination, 0);
+						}
 						imagedestroy($tempImg);
 						imagedestroy($tempColor);
 						$fileUploaded = true;
 					}
 				} elseif ($avatarExt == 'svg') {
-					foreach (glob(FILEROOT.'/ucp/avatars/'.$user->userID.'.*') as $oldFile)
+					foreach (glob(FILEROOT.'/ucp/avatars/'.$user->userID.'.*') as $oldFile) {
 						unlink($oldFile);
+					}
 					move_uploaded_file($_FILES['file']['tmp_name'], FILEROOT."/ucp/avatars/{$user->userID}.svg");
 					$fileUploaded = true;
 				}
 
-				if ($avatarExt == '')
+				if ($avatarExt == '') {
 					$avatarExt = null;
+				}
 				$user->updateUsermeta('avatarExt', $avatarExt, true);
 				$avatarUploaded = true;
 			}
 
 //			$user->updateUsermeta('showAvatars', isset($data->showAvatars)?1:0);
-			if ($details->gender == 'n')
+			if ($details['gender'] == 'n') {
 				$gender = '';
-			else
-				$gender = $details->gender == 'm'?'m':'f';
+			} else {
+				$gender = $details['gender'] == 'm'?'m':'f';
+			}
 			$user->updateUsermeta('gender', $gender);
-			$birthday = intval($details->birthday->date->year).'-'.(intval($details->birthday->date->month) <= 9?'0':'').intval($details->birthday->date->month).'-'.(intval($details->birthday->date->day) <= 9?'0':'').intval($details->birthday->date->day);
-			if (preg_match('/^[12]\d{3}-[01]\d-[0-3]\d$/', $birthday))
+			$birthday = intval($details['birthday']['date']['year']) . '-' . (intval($details['birthday']['date']['month']) <= 9 ? '0' : '') . intval($details['birthday']['date']['month']) . '-' . (intval($details['birthday']['date']['day']) <= 9 ? '0' : '') . intval($details['birthday']['date']['day']);
+			if (preg_match('/^[12]\d{3}-[01]\d-[0-3]\d$/', $birthday)) {
 				$user->updateUsermeta('birthday', $birthday);
-			$user->updateUsermeta('showAge', $details->birthday->showAge?1:0);
-			$user->updateUsermeta('location', sanitizeString($details->location));
-			$user->updateUsermeta('twitter', sanitizeString($details->twitter));
-			$user->updateUsermeta('stream', sanitizeString($details->stream));
-			$user->updateUsermeta('games', sanitizeString($details->games));
-			$user->updateUsermeta('pmMail', intval($details->pmMail)?1:0);
-			$user->updateUsermeta('newGameMail', intval($details->newGameMail)?1:0);
-			$user->updateUsermeta('gmMail', intval($details->gmMail)?1:0);
+			}
+			$user->updateUsermeta('showAge', $details['birthday']['showAge'] ? 1 : 0);
+			$user->updateUsermeta('location', sanitizeString($details['location']));
+			$user->updateUsermeta('twitter', sanitizeString($details['twitter']));
+			$user->updateUsermeta('stream', sanitizeString($details['stream']));
+			$user->updateUsermeta('games', sanitizeString($details['games']));
+			$user->updateUsermeta('pmMail', intval($details['pmMail'])?1:0);
+			$user->updateUsermeta('newGameMail', intval($details['newGameMail'])?1:0);
+			$user->updateUsermeta('gmMail', intval($details['gmMail'])?1:0);
 
 			$errors = array();
-			$oldPass = $newPass->oldPass;
-			$password1 = $newPass->password1;
-			$password2 = $newPass->password2;
+			$oldPass = $newPass['oldPass'];
+			$password1 = $newPass['password1'];
+			$password2 = $newPass['password2'];
 			if (strlen($password1) && strlen($password2) ) {
-				if (!$user->validate($oldPass) && !$user->checkACP('users', false))
+				if (!$user->validate($oldPass) && !$user->checkACP('users', false)) {
 					$errors[] = 'wrongPass';
-				if (strlen($password1) < 6)
+				}
+				if (strlen($password1) < 6) {
 					$errors[] = 'passShort';
-				if (strlen($password1) > 32)
+				}
+				if (strlen($password1) > 32) {
 					$errors[] = 'passLong';
-				if ($password1 != $password2)
+				}
+				if ($password1 != $password2) {
 					$errors[] = 'passMismatch';
+				}
 
-				if (!sizeof($errors))
+				if (!sizeof($errors)) {
 					$user->updatePassword($password1);
+				}
 			}
 
-			if (in_array($details->postSide, array('l', 'r', 'c')))
-				$postSide = $details->postSide;
-			else
+			if (in_array($details['postSide'], array('l', 'r', 'c'))) {
+				$postSide = $details['postSide'];
+			} else {
 				$postSide = 'l';
+			}
 			$user->updateUsermeta('postSide', $postSide);
 
 			$return = array();
-			if (sizeof($errors))
+			if (sizeof($errors)) {
 				$return['passErrors'] = $errors;
-			else
+			} else {
 				$return['success'] = true;
-			if ($avatarUploaded)
+			}
+			if ($avatarUploaded) {
 				$return['avatarUploaded'] = true;
+			}
 			displayJSON($return);
 		}
 
--- javascript/ucp/ucp.js
+++ javascript/ucp/ucp.js
@@ -2,17 +2,29 @@ controllers.controller('ucp', ['$scope', '$http', 'CurrentUser', 'UsersService',
 	$scope.$emit('pageLoading');
 	CurrentUser.load().then(function () {
 		$scope.CurrentUser = CurrentUser.get();
-		$scope.admin = !isUndefined($scope.CurrentUser.acpPermissions) && $scope.CurrentUser.acpPermissions != null && ($scope.CurrentUser.acpPermissions.indexOf('users') || $scope.CurrentUser.acpPermissions.indexOf('all'))?true:false;
+		$scope.admin = !isUndefined($scope.CurrentUser.acpPermissions) && $scope.CurrentUser.acpPermissions !== null && ($scope.CurrentUser.acpPermissions.indexOf('users') || $scope.CurrentUser.acpPermissions.indexOf('all'))?true:false;
 		var userID = null;
 		var pathElements = getPathElements();
-		if (!isUndefined(pathElements[1])) 
+		if (!isUndefined(pathElements[1])) {
 			userID = parseInt(pathElements[1]);
-		else 
+		} else {
 			userID = $scope.CurrentUser.userID;
-		if ($scope.CurrentUser.userID != userID && (typeof $scope.CurrentUser.acpPermissions == 'undefined' || $scope.CurrentUser.acpPermissions == null || ($scope.CurrentUser.acpPermissions.indexOf('users') == -1 && $scope.CurrentUser.acpPermissions.indexOf('all') == -1))) 
-			window.location.href = '/user/' + (userID != null?userID + '/':'');
+		}
+		if (
+			$scope.CurrentUser.userID != userID &&
+			(
+				typeof $scope.CurrentUser.acpPermissions == 'undefined' ||
+				$scope.CurrentUser.acpPermissions === null ||
+				(
+					$scope.CurrentUser.acpPermissions.indexOf('users') == -1 &&
+					$scope.CurrentUser.acpPermissions.indexOf('all') == -1
+				)
+			)
+		) {
+			window.location.href = '/user/' + (userID !== null?userID + '/':'');
+		}
 		$scope.user = null;
-		$scope.newPass = { 'oldPassword': '', 'password1': '', 'password2': '' }
+		$scope.newPass = { 'oldPassword': '', 'password1': '', 'password2': '' };
 		$scope.newAvatar = null;
 		$scope.avatarTime = new Date().getTime();
 
@@ -33,17 +45,22 @@ controllers.controller('ucp', ['$scope', '$http', 'CurrentUser', 'UsersService',
 
 		$scope.passMismatch = false;
 		$scope.samePass = function () {
-			if ($scope.newPass.password1.length >= 6 && $scope.newPass.password1.length <= 32) 
+			if ($scope.newPass.password1.length >= 6 && $scope.newPass.password1.length <= 32) {
 				$scope.passMismatch = $scope.newPass.password1 != $scope.newPass.password2;
-			else if ($scope.newPass.password1.length < 6 && $scope.newPass.password1.length > 32) 
+			} else if ($scope.newPass.password1.length < 6 && $scope.newPass.password1.length > 32) {
 				$scope.passMismatch = false;
+			}
 		};
 
 		$scope.save = function () {
-			UsersService.save({ 'details': $scope.user, 'newPass': $scope.newPass }, $scope.newAvatar).then(function (data) {
+			UsersService.save({
+				'details': $scope.user,
+				'newPass': $scope.newPass
+			}, $scope.newAvatar).then(function (data) {
 				data = data.data;
-				if (data.avatarUploaded) 
+				if (data.avatarUploaded) {
 					$scope.avatarTime = new Date().getTime();
+				}
 			});
 		};
 	});
