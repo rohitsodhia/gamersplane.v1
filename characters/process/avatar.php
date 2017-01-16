453bd3b8139b5202594d45a2e47ebe10dd7d6705
Rohit Sodhia <rohit@rhovisions.com>
2017-01-16 15:57:05 -0500

    Hide unlink error

--- characters/process/avatar.php
+++ characters/process/avatar.php
@@ -13,22 +13,22 @@
 		}
 
 		if ($charPermissions && (($_FILES['avatar'] && $_FILES['avatar']['error'] == 0 && $_FILES['avatar']['size'] > 15 && $_FILES['avatar']['size'] < 1048576) || $_POST['delete'])) {
-			unlink(FILEROOT."/characters/avatars/{$characterID}.jpg");
+			@unlink(FILEROOT."/characters/avatars/{$characterID}.jpg");
 			if (isset($_FILES['avatar'])) {
 				$avatarExt = trim(end(explode('.', strtolower($_FILES['avatar']['name']))));
 				if ($avatarExt == 'jpeg') $avatarExt = 'jpg';
 				if (in_array($avatarExt, array('jpg', 'gif', 'png'))) {
 					$maxWidth = 150;
 					$maxHeight = 150;
-					
+
 					list($imgWidth, $imgHeight, $imgType) = getimagesize($_FILES['avatar']['tmp_name']);
 					if (image_type_to_mime_type($imgType) == 'image/jpeg' || image_type_to_mime_type($imgType) == 'image/pjpeg') $tempImg = imagecreatefromjpeg($_FILES['avatar']['tmp_name']);
 					elseif (image_type_to_mime_type($imgType) == 'image/gif') $tempImg = imagecreatefromgif($_FILES['avatar']['tmp_name']);
 					elseif (image_type_to_mime_type($imgType) == 'image/png') $tempImg = imagecreatefrompng($_FILES['avatar']['tmp_name']);
-					
+
 					$xRatio = $maxWidth / $imgWidth;
 					$yRatio = $maxHeight / $imgHeight;
-					
+
 					if ($imgWidth <= $maxWidth && $imgHeight <= $maxHeight) {
 						$finalWidth = $imgWidth;
 						$finalHeight = $imgHeight;
@@ -39,12 +39,12 @@
 						$finalWidth = ceil($yRatio * $imgWidth);
 						$finalHeight = $maxHeight;
 					}
-					
+
 					$tempColor = imagecreatetruecolor($finalWidth, $finalHeight);
 					imagealphablending($tempColor, false);
 					imagesavealpha($tempColor,true);
 					imagecopyresampled($tempColor, $tempImg, 0, 0, 0, 0, $finalWidth, $finalHeight, $imgWidth, $imgHeight);
-					
+
 					$destination = FILEROOT."/characters/avatars/{$characterID}.jpg";
 					if (file_exists($destination)) unlink($destination);
 					imagejpeg($tempColor, $destination, 100);
