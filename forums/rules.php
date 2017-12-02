17374f0bf99be6ebc42009a9897c6aeb8cb4240d
Rohit Sodhia <rohit@rhovisions.com>
2017-12-02 18:09:16 -0500

    Add media discussion rules

--- forums/rules.php
+++ forums/rules.php
@@ -1,6 +1,6 @@
 <? require_once(FILEROOT.'/header.php'); ?>
 		<h1>Forum Rules</h1>
-		
+
 		<h2>General guidelines for the forums</h2>
 		<p>Gamers Plane is open anyone, regardless of age, and as such, members are expected to follow certain guidelines.</p>
 		<ol>
@@ -22,14 +22,22 @@
 			<li>Any actions meant to harm GP, its server, or its content, or actions meant to circumvent its security are prohibited.</li>
 		</ol>
 		<p>I'm sad I have to actually put this as a rule, but if you join Gamers' Plane just to promote material on other play-by-post sites, I will just go ahead and ban your account. I don't mind people talking about other sites, if you're an active participant here. But coming here just to get people away from here is crass and rude.</p>
-		
+
 		<hr>
 		<h2>Rules for specific forums</h2>
 		<p>The General Discussion forum is for most discussions that don't belong in other forums. Discussions on games should go in the appropriate Game Discussions forum, ideas/problems about the site under Site discussions, and game requests under Games Tavern. Anything else goes in General Discussions.</p>
 		<p>Threads/posts should have a content and purpose, be it to introduce yourself or start a discussion on a topic.</p>
-		
+
 		<p>We will allow the rules for language used in Game Forums (other then the Games Tavern) to be looser, as long as it is clearly printed at the root forum/game details. However, players are still expected to treat each other with respect, to not use slurs against other players, etc.</p>
-		
+
+		<hr>
+		<h2>Media discussion rules</h2>
+		<p>Everyone should have the chance to see a new movie, read a new book, etc. But people who've already done so may also want to talk about new media, as that's half the fun! So if something new comes out that you want to talk about but others may not yet have gotten the chance to check out, follow these rules based on when it came out:</p>
+		<ul>
+			<li>< 6 months since release - Any thread about a recent release that includes any information that easily available publically should be marked with <strong>[Spoilers]</strong> at the start of the thread title. If it's just a thread mentioning a release, or talking about what's easy to find, the tag isn't necessary. If you're in doubt, add the tag.</li>
+			<li>6 months or more since release - At a certain point, people have had a chance to check it out. If you start up a thread about something that came out half a year ago or more, no tags are necessary. However, if whatever it is exists in some sort of perpetual state (like a game that releases content every month over the course of a year), be mindful of that. Again, if in doubt, add a tag.</li>
+		</ul>
+
 		<hr>
 		<h2>Moderator responsibilities</h2>
 		<p>The moderators can decide whether a post meets these guidelines or not. You are welcome to contest a decision via private message, but simply whining/arguing may raise your punishment.</p>
