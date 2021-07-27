d8d64067cb7b25210c6dbc9488cb706777463a86
Adam <adamdnd@devprocess.com>
2021-07-27 13:36:38 +0100

    Add a link to char sheet next to "Post as" option

--- javascript/forums/unsaved-work.js
+++ javascript/forums/unsaved-work.js
@@ -11,3 +11,21 @@ window.onbeforeunload = function ()
         return "You haven't submitted your post. Click OK to continue without saving or Cancel to go back and save your post.";
     }
 };
+
+$(function() {
+
+    var updateCharLink=function(postAs){
+        var charPost=$('#fm_characters .charid-'+postAs.val());
+        var charSheetLink=$('#charSheetLink').html('');
+        if(charPost.length>0){
+            charPost=charPost.eq(0);
+            $('<a target="_blank"></a>').text(charPost.text()).attr('href',charPost.attr('href')).appendTo(charSheetLink);
+        }
+    };
+
+    $('select[name="postAs"]').on('change',function(){
+        updateCharLink($(this));
+    });
+    
+    updateCharLink($('select[name="postAs"]'));
+});
