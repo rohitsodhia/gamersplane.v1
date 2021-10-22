619b7c0c25a8629e4c23e3e5401771864fc5a488
Adam <adamdnd@devprocess.com>
2021-10-22 15:19:17 +0100

    Move thread now comes in on a post action

--- forums/process/modThread.php
+++ forums/process/modThread.php
@@ -2,22 +2,23 @@
 	include(FILEROOT.'/includes/forums/ForumPermissions.class.php');
 
 	$threadID = intval($_POST['threadID']);
-	
+
 	$forumID = $mysql->query('SELECT forumID FROM threads WHERE threadID = '.$threadID);
 	$forumID = $forumID->fetchColumn();
-	
+
 	$permissions = ForumPermissions::getPermissions($currentUser->userID, $forumID, 'moderate');
 
 	if ($permissions[$forumID]['moderate']) {
 		$action = '';
 		if (isset($_POST['lock'])) $action = 'lock';
 		elseif (isset($_POST['sticky'])) $action = 'sticky';
+		elseif (isset($_POST['move'])) { header("Location: /forums/moveThread/{$threadID}/"); exit; }
 		elseif (isset($_POST['action']) && in_array($_POST['action'], array('lock', 'sticky', 'move'))) $action = $_POST['action'];
-		
+
 		if ($action == 'lock') $mysql->query('UPDATE threads SET locked = locked ^ 1 WHERE threadID = '.$threadID);
 		elseif ($action == 'sticky') $mysql->query('UPDATE threads SET sticky = sticky ^ 1 WHERE threadID = '.$threadID);
 		elseif ($action == 'move') { header("Location: /forums/moveThread/{$threadID}/"); exit; }
 	}
-	
+
 	header("Location: /forums/thread/{$threadID}/");
 ?>
