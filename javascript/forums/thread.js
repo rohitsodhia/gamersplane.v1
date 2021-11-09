728ed09f0db9eceb64585d139dfd1e6f13313224
Adam <adamdnd@devprocess.com>
2021-11-09 06:22:11 +0000

    Game, forum menu

--- javascript/forums/thread.js
+++ javascript/forums/thread.js
@@ -95,4 +95,41 @@ $(function() {
 		});
 	});
 
+//forums menu
+	var newMenu=$('<li id="fm_forumthreads"><a class="menuLink">Forum</a><ul class="submenu"></li></li>').appendTo($('#fixedMenu_window .leftCol'));
+	$('.menuLink',newMenu).on('click',function(){
+		var submenu=$('.submenu',$(this).parent());
+		$('#fixedMenu .submenu.fm_smOpen').not(submenu).slideUp(250).removeClass('fm_smOpen');
+		submenu.slideToggle(250).toggleClass('fm_smOpen');
+	});
+
+	var forumsLink=$('#threadMenu #breadcrumbs a').last().attr('href');
+	$.get( forumsLink, function( data ) {
+		var dataObj=$(data);
+		var forums=$('.forumList .tr',dataObj);
+		var subMenu=$('.submenu',newMenu);
+		if(forums.length>0)
+		{
+			$('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Forums');
+			for(var i=0;i<forums.length;i++)
+			{
+				$('<li></li>').html($('.name',forums[i]).html()).appendTo(subMenu);
+			}
+		}
+		var threads=$('.threadList .tr',dataObj);
+		if(threads.length>0)
+		{
+			$('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Threads');
+			for(var i=0;i<threads.length;i++)
+			{
+				var menuItem=$('<li><a></a></li>').appendTo(subMenu);
+				var threadText=$('.td.threadInfo>a',threads[i]);
+				$('a',menuItem).attr('href',threadText.attr('href')).text(threadText.text());
+			}
+
+		}
+
+	});
+
+
 });
