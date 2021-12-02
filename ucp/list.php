be8e75d019465700aecadf64a2d3ad9242a7643f
Adam <adamdnd@devprocess.com>
2021-12-02 08:41:07 +0000

    Sort gamers list

--- ucp/list.php
+++ ucp/list.php
@@ -2,19 +2,26 @@
 	require_once(FILEROOT.'/header.php'); ?>
 		<h1 class="headerbar"><i class="ra ra-gamers-plane"></i> GP's Gamers <input type="text" ng-model="filter.search" placeholder="Search..." class="headerSearch"/></h1>
 
-		<p class="hbMargined" hb-margined>
+		<p class="hbMargined gamerListOptions" hb-margined>
 			<label>
+				<span class="labelText mob-hide">Show inactive users</span>
+				<span class="labelText non-mob-hide">Inactive</span>
 				<pretty-checkbox checkbox="showInactive"></pretty-checkbox>
-				<span class="labelText">Show inactive users</span>
 			</label>
 			<label>
+				<span class="labelText mob-hide">Looking for a game</span>
+				<span class="labelText non-mob-hide">LFG</span>
 				<pretty-checkbox checkbox="lookingForAGame"></pretty-checkbox>
-				<span class="labelText">Looking for a game</span>
+			</label>
+			<label>
+				<span class="labelText mob-hide">Sort by</span>
+				<span class="labelText non-mob-hide">Sort</span>
+				<select class="notPretty" ng-model="ordering"><option value="0">Name</option><option value="2">Newest</option><option value="1">Oldest</option></select>
 			</label>
 			<paginate num-items="pagination.numItems" items-per-page="pagination.itemsPerPage" current="pagination.current"></paginate>
 		</p>
 		<ul id="gamersList" hb-margined>
-			<li ng-repeat="user in users| filter:filterItems| paginateItems: 25:(pagination.current - 1) * 25" ng-class="{ 'last': $index % 5 == 4 }">
+			<li ng-repeat="user in users| filter:filterItems| orderBy:sortOrder | paginateItems: 25:(pagination.current - 1) * 25" ng-class="{ 'last': $index % 5 == 4 }">
 				<div class="onlineIndicator" ng-class="{ 'online': user.online, 'offline': !user.online }"></div>
 				<div class="lfgIndicator" ng-if="user.lfgStatus"><i class="ra ra-health"></i></div>
 				<a href="/user/{{user.userID}}/" class="avatar">
