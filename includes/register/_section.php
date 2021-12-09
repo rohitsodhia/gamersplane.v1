c2fa26c6f5d6e22217bbf0e10d669d97b904d0e1
Rohit Sodhia <sodhia.rohit@gmail.com>
2021-12-08 22:48:11 -0500

    Missed some emails to set pathBase for

--- includes/register/_section.php
+++ includes/register/_section.php
@@ -1,9 +1,10 @@
 <?
 	function sendActivationEmail($email, $username) {
+		$pathBase = 'https://'.getenv('APP_URL');
 		$message = "Thank you for registering for Gamers Plane!\n\n";
 		$message .= "Please click on the following link to activate your account:\n";
-		$message .= '<a href="http://gamersplane.com/register/activate/'.md5($username)."\">Activate account</a>\n";
-		$message .= 'Or copy and paste this URL into your browser: http://gamersplane.com/register/activate/'.md5($username)."/\n\n";
+		$message .= '<a href="'.$pathBase.'/register/activate/'.md5($username)."\">Activate account</a>\n";
+		$message .= 'Or copy and paste this URL into your browser: '.$pathBase.'/register/activate/'.md5($username)."/\n\n";
 		$message .= 'Please do not respond to this email, as it will be ignored';
 		$mailSent = false;
 		if (strtolower($_SERVER['HTTP_HOST']) == 'gamersplane.com') {
--- login/process/requestReset.php
+++ login/process/requestReset.php
@@ -1,25 +1,26 @@
 <?
 	if (isset($_POST['submit'])) {
 		$email = sanitizeString($_POST['email'], '+lower');
-		
+
 		$userCheck = $mysql->prepare('SELECT userID FROM users WHERE LOWER(email) = ? AND activatedOn IS NOT NULL');
 		$userCheck->execute(array($email));
 		if ($userCheck->rowCount()) {
 			$user = new User($email);
 			$validationStr = substr(md5($email.substr($user->password, 3).$user->activatedOn), 4, 13);
+			$pathBase = 'https://'.getenv('APP_URL');
 			$body = "Gamers Plane Password Reset
 
 ---------------------------------------------------
 
 To reset your password, please go to
 
-http://gamersplane.com/login/resetPass/?email={$email}&validate={$validationStr}
+{$pathBase}/login/resetPass/?email={$email}&validate={$validationStr}
 
 It will take you to a page where you can enter a new password of your choice.";
 			mail($email, 'Gamers Plane Password Reset', $body, "From: contact@gamersplane.com\r\nReply-To: contact@gamersplane.com");
 			header('Location: /login/requestReset/?success=1'.(isset($_POST['modal'])?'&modal=1':''));
-		} else 
+		} else
 			header('Location: /login/requestReset/?invalidEmail=1'.(isset($_POST['modal'])?'&modal=1':''));
-	} else 
+	} else
 		header('Location: /login/requestReset/'.(isset($_POST['modal'])?'?modal=1':''));
 ?>
