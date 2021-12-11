b53095e1de0b7b6774d1eaef5da9ca1c7a5cdfeb
Adam <adamdnd@devprocess.com>
2021-12-11 14:04:24 +0000

    Deeper threads menu for subforums

--- javascript/forums/thread.js
+++ javascript/forums/thread.js
@@ -123,33 +123,44 @@ $(function() {
 		submenu.slideToggle(250).toggleClass('fm_smOpen');
 	});
 
-	var forumsLink=$('#threadMenu #breadcrumbs a').last().attr('href');
-	$.get( forumsLink, function( data ) {
-		var dataObj=$(data);
-		var forums=$('.forumList .tr',dataObj);
-		var subMenu=$('.submenu',newMenu);
-		if(forums.length>0)
-		{
-			$('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Forums');
-			for(var i=0;i<forums.length;i++)
-			{
-				$('<li></li>').html($('.name',forums[i]).html()).appendTo(subMenu);
+
+
+	var loadForumLinks=function(forumLink){
+		if(forumLink.length>0){
+			var forumUrl=forumLink.attr('href');
+			if(forumUrl.endsWith('/0')||forumUrl.endsWith('/2')){
+				loadForumLinks(forumLink.next('a'));
 			}
-		}
-		var threads=$('.threadList .tr',dataObj);
-		if(threads.length>0)
-		{
-			$('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Threads');
-			for(var i=0;i<threads.length;i++)
-			{
-				var menuItem=$('<li><a></a></li>').appendTo(subMenu);
-				var threadText=$('.td.threadInfo>a',threads[i]);
-				$('a',menuItem).attr('href',threadText.attr('href')).text(threadText.text());
+			else{
+				$.get( forumUrl, function( data ) {
+					var dataObj=$(data);
+					var forums=$('.forumList .tr',dataObj);
+					var subMenu=$('.submenu',newMenu);
+					if(forums.length>0)
+					{
+						$('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Forums: '+forumLink.text());
+						for(var i=0;i<forums.length;i++)
+						{
+							$('<li></li>').html($('.name',forums[i]).html()).appendTo(subMenu);
+						}
+					}
+					var threads=$('.threadList .tr',dataObj);
+					if(threads.length>0)
+					{
+						$('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Threads: '+forumLink.text());
+						for(var i=0;i<threads.length;i++)
+						{
+							var menuItem=$('<li><a></a></li>').appendTo(subMenu);
+							var threadText=$('.td.threadInfo>a',threads[i]);
+							$('a',menuItem).attr('href',threadText.attr('href')).text(threadText.text());
+						}
+					}
+					loadForumLinks(forumLink.next('a'));
+				});
 			}
 
 		}
+	};
 
-	});
-
-
+	loadForumLinks($('#threadMenu #breadcrumbs a').first());
 });
