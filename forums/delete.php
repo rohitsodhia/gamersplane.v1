ff9b094aa93484a35e55a87b229e64bcedc49d1c
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-02-01 01:10:29 +0000

    Allow delete own post in public comment thread (#52)

--- forums/delete.php
+++ forums/delete.php
@@ -4,15 +4,15 @@
 	$post = new Post($postID);
 	$threadManager = new ThreadManager($post->getThreadID());
 	$deleteType = ($threadManager->getFirstPostID() == $postID)?'thread':'post';
-	
-	if (($post->getAuthor('userID') != $currentUser->userID && !$threadManager->getPermissions('moderate')) || ($post->getAuthor('userID') == $currentUser->userID && $threadManager->getFirstPostID() != $postID && !$threadManager->getPermissions('deletePost')) || ($post->getAuthor('userID') != $currentUser->userID && $threadManager->getFirstPostID() == $postID && !$threadManager->getPermissions('deleteThread'))) { header('Location: /forums/thread/'.$postInfo['threadID']); exit; }
+
+	if (($post->getAuthor('userID') != $currentUser->userID && !$threadManager->getPermissions('moderate')) || ($post->getAuthor('userID') == $currentUser->userID && $threadManager->getFirstPostID() != $postID && !$threadManager->getPermissions('deletePost') && !$threadManager->getThreadProperty('states[publicPosting]')) || ($post->getAuthor('userID') != $currentUser->userID && $threadManager->getFirstPostID() == $postID && !$threadManager->getPermissions('deleteThread'))) { header('Location: /forums/thread/'.$postInfo['threadID']); exit; }
 ?>
 <? require_once(FILEROOT.'/header.php'); ?>
 		<h1 class="headerbar">Delete <?=ucwords($deleteType)?></h1>
-		
+
 		<p class="alignCenter">Are you sure you wanna delete this <?=$deleteType?>?</p>
 		<p class="alignCenter">This cannot be reversed!</p>
-		
+
 		<form method="post" action="/forums/process/delete/" class="alignCenter">
 			<input type="hidden" name="postID" value="<?=$postID?>">
 			<button type="submit" name="delete" class="fancyButton">Delete</button>
