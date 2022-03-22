c65da347c762522ed7b924df120830f11a180436
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-03-22 22:10:36 +0000

    Removing most common GP warnings from error log (#85)

--- includes/forums/ForumManager.class.php
+++ includes/forums/ForumManager.class.php
@@ -193,7 +193,7 @@ class ForumManager
 			$forumID = $this->currentForum;
 		}
 
-		if (sizeof($this->forums[$forumID]->children) || $this->forums[$forumID]->permissions['read']) {
+		if ((is_array($this->forums[$forumID]->children) && sizeof($this->forums[$forumID]->children)) || $this->forums[$forumID]->permissions['read']) {
 			return true;
 		} else {
 			return false;
