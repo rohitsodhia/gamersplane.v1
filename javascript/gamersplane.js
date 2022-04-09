d7cb9aa5dafa28597d8432854f55d09dd707e682
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-04-09 23:14:19 +0100

    Tweak/charsheet hotkeys (#98)
    * Hotkeys for inplace character sheets textareas
    
    * Single input update, onComplete in wrong place
    
    * css fix zoomed checkboxes

--- javascript/gamersplane.js
+++ javascript/gamersplane.js
@@ -1043,3 +1043,58 @@ app.config(['$httpProvider', function ($httpProvider) {
 	});
 }]);
 var controllers = angular.module('controllers', []);
+
+var ImgurHelper=(function(){
+	var uploadFiles=function(file,onUploaded){
+		var imgurApi = 'https://api.imgur.com/3/image';
+		var apiKey = '7de684608de179a';
+
+		var settings = {
+			url: imgurApi,
+			type: 'POST',
+			async: false, crossDomain: true, processData: false, contentType: false,
+			headers: {
+				Authorization: 'Client-ID ' + apiKey,
+				Accept: 'application/json',
+			},
+			mimeType: 'multipart/form-data',
+		};
+
+		var formData = new FormData();
+		formData.append('image', file);
+		settings.data = formData;
+
+		$.ajax(settings).done(function (response) {
+			var responseObj = JSON.parse(response);
+			$('#messageTextArea').focus();
+			onUploaded(responseObj.data.link);
+		}).fail(function (errorReason) {
+			if(errorReason && errorReason.responseText){
+				var responseObj = JSON.parse(errorReason.responseText);
+				alert(responseObj.data.error.message);
+			}
+			else{
+				alert('Unable to upload file.');
+			}
+		});
+	};
+
+	var uploadFromClipboard=function(ev, onUploaded){
+		var clipboard=(ev.originalEvent.clipboardData || window.clipboardData);
+		if(clipboard && clipboard.items){
+			for(var i=0;i<clipboard.items.length;i++){
+				var item= clipboard.items[i];
+				if (item.type.indexOf("image") === 0){
+					var blob = item.getAsFile();
+					ImgurHelper.uploadFiles(blob,onUploaded);
+					ev.preventDefault();
+				}
+			}
+		}
+	};
+
+	return {
+        uploadFiles: uploadFiles,
+		uploadFromClipboard:uploadFromClipboard
+    };
+})();
