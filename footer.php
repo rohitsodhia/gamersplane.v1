48a5092ecf6ab153a05515f7ce0de46480e1c369
Rohit Sodhia <sodhia.rohit@gmail.com>
2023-06-25 21:55:35 -0400

    Move google analytics tag to header

--- footer.php
+++ footer.php
@@ -51,15 +51,5 @@
 <?php	} ?>
 
 <?php	require_once(FILEROOT . '/javascript/js.php'); ?>
-
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-46259019-1', 'gamersplane.com');
-	ga('send', 'pageview');
-</script>
 </body>
 </html>
