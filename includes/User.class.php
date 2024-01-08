0d738f094564495f4ecfc457c782a042a1eda197
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-01-07 19:46:02 -0500

    Wrap string in quotes

--- includes/User.class.php
+++ includes/User.class.php
@@ -185,7 +185,7 @@
 		public function getAllUsermeta() {
 			$mysql = DB::conn('mysql');
 
-			$metaValues = $mysql->query("SELECT metaKey, metaValue FROM usermeta WHERE userID = {$this->userID} AND autoload = 0");
+			$metaValues = $mysql->query("SELECT metaKey, metaValue FROM usermeta WHERE userID = {$this->userID} AND autoload = '0'");
 			foreach ($metaValues as $metas) {
 				if (is_string($metas['metaKey']) && strlen($metas['metaKey']) > 4 && substr($metas['metaKey'], 0, 2) == 'a:') {
 					$metas['metaKey'] = unserialize($metas['metaKey']);
