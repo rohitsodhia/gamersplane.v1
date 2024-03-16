ac5317b640e4d1392bc6c3daca471a0f6e5aea4b
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-03-16 18:25:46 -0400

    Remove opendkim portion of postfix entrypoint

--- docker/postfix/docker-entrypoint.sh
+++ docker/postfix/docker-entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -x
 
-mkdir -p /var/run/opendkim
-opendkim -u opendkim
-
 sed -i -r -e "s/^(myhostname =) gamersplane.com$/\1 $POSTFIX_MYHOSTNAME/" /etc/postfix/main.cf
 postalias /etc/postfix/aliases
 postfix start-fg
