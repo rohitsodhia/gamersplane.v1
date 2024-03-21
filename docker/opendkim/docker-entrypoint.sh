850c43c4724d3b607f461d1fdf52360d92e36060
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-03-21 14:57:40 -0400

    Opendkim fast follow (#141)
    * A bunch of changes based on prod and extra
    
    * Move vaguegm updates

--- docker/opendkim/docker-entrypoint.sh
+++ docker/opendkim/docker-entrypoint.sh
@@ -4,5 +4,10 @@ set -x
 
 syslogd
 
+CONF_FILE="/etc/opendkim/opendkim.conf"
+sed -i -r -e "s/^(Domain\s+)\sgamersplane.com$/\1 $POSTFIX_MYHOSTNAME/" $CONF_FILE
+sed -i -r -e "s/^(Selector\s+)\skey_selector$/\1 $OPENDKIM_SELECTOR/" $CONF_FILE
+chmod 600 $CONF_FILE
+
 mkdir -p /var/run/opendkim
 opendkim -u opendkim -f
--- docker/postfix/docker-entrypoint.sh
+++ docker/postfix/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -x
 
-sed -i -r -e "s/^(myhostname =) gamersplane.com$/\1 $POSTFIX_MYHOSTNAME/" /etc/postfix/main.cf
+CONF_FILE="/etc/postfix/main.cf"
+sed -i -r -e "s/^(myhostname =) gamersplane.com$/\1 $POSTFIX_MYHOSTNAME/" $CONF_FILE
+chmod 600 $CONF_FILE
+
 postalias /etc/postfix/aliases
 postfix start-fg
