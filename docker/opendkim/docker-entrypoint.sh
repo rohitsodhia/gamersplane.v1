8c7f800b1c5d54d4d3a46d26a88242c12be0c6a3
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-03-24 17:44:43 -0400

    Fix opendkim key permissions (#143)

--- docker/opendkim/docker-entrypoint.sh
+++ docker/opendkim/docker-entrypoint.sh
@@ -8,6 +8,7 @@ CONF_FILE="/etc/opendkim/opendkim.conf"
 sed -i -r -e "s/^(Domain\s+)\sgamersplane.com$/\1 $POSTFIX_MYHOSTNAME/" $CONF_FILE
 sed -i -r -e "s/^(Selector\s+)\skey_selector$/\1 $OPENDKIM_SELECTOR/" $CONF_FILE
 chmod 600 $CONF_FILE
+chmod 600 /etc/opendkim/keys/key.private
 
 mkdir -p /var/run/opendkim
 opendkim -u opendkim -f
