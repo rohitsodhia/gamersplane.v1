a46e50ca80da4980b5b435b62b42ed38ee51bf22
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-07-11 22:18:11 -0400

    Sort users and characters in games

--- api/games.class.php
+++ api/games.class.php
@@ -181,7 +181,7 @@ class games
 			}
 		}
 
-		$getPlayers = $mysql->query("SELECT players.userID, users.username, players.approved, players.isGM FROM players INNER JOIN users ON players.userID = users.userID WHERE players.gameID = {$gameID}");
+		$getPlayers = $mysql->query("SELECT players.userID, users.username, players.approved, players.isGM FROM players INNER JOIN users ON players.userID = users.userID WHERE players.gameID = {$gameID} ORDER GY players.isGM DESC, users.username");
 		$players = [];
 		$playerIDs = [];
 		foreach ($getPlayers->fetchAll() as $player) {
@@ -199,7 +199,7 @@ class games
 				$gameInfo['approvedPlayers']++;
 			}
 		}
-		$getCharacters = $mysql->query("SELECT characterID, userID, label, approved FROM characters WHERE gameID = {$gameID} AND userID IN (". implode(', ', $playerIDs) .")");
+		$getCharacters = $mysql->query("SELECT characterID, userID, label, approved FROM characters WHERE gameID = {$gameID} AND userID IN (". implode(', ', $playerIDs) .") ORDER BY label");
 		$characters = [];
 		foreach ($getCharacters->fetchAll() as $character) {
 			$userID = $character['userID'];
