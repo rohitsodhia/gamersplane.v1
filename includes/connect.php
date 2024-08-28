b3496dc72d9a609e80b321c02dceebd37e8058e5
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-08-27 22:45:11 -0400

    Set charset for connection

--- includes/connect.php
+++ includes/connect.php
@@ -4,7 +4,7 @@
 	$dbPassword = getenv('MYSQL_PASSWORD');
 	$dbName     = getenv('MYSQL_DATABASE');
 
-	$mysql = new PDO("mysql:host=$dbHostname;dbname=$dbName", $dbUsername, $dbPassword);
+	$mysql = new PDO("mysql:host=$dbHostname;dbname=$dbName;charset=utf8mb4", $dbUsername, $dbPassword);
 	$mysql->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	$mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$mysql->query('SET time_zone="GMT"');
