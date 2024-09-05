ce3be95b0775b0e46afabc467e4264a5d3f0ea5c
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-09-05 15:52:34 -0400

    Fix query to get favorite characters, extend header char/game dropdowns back to 10

--- api/users.class.php
+++ api/users.class.php
@@ -164,7 +164,7 @@
 				displayJSON(['failed' => true]);
 			}
 
-			$getCharacters = $mysql->query("SELECT DISTINCT characters.characterID, characters.label, characters.system, IF(favorites.userID, 1, 0) isFavorite FROM characters LEFT JOIN characterLibrary_favorites favorites ON characters.characterID = favorites.characterID WHERE (characters.userID = {$currentUser->userID} OR favorites.userID = {$currentUser->userID}) AND characters.retired IS NULL ORDER BY isFavorite DESC LIMIT 6");
+			$getCharacters = $mysql->query("SELECT DISTINCT characters.characterID, characters.label, characters.system, IF(favorites.userID, 1, 0) isFavorite FROM characters LEFT JOIN characterLibrary_favorites favorites ON characters.characterID = favorites.characterID AND favorites.userID = {$currentUser->userID} WHERE characters.userID = {$currentUser->userID} AND characters.retired IS NULL ORDER BY isFavorite DESC LIMIT 10");
 
 			$characters = [];
 			$hasFavorites = FALSE;
@@ -180,7 +180,7 @@
 				$characters[] = $char;
 			}
 
-			$games = $mysql->query("SELECT games.gameID, games.title, games.forumID, IF(players.isGM, players.isGM, FALSE) isGM, IF(players.userID, TRUE, FALSE) isPlayer FROM games LEFT JOIN players ON games.gameID = players.gameID AND players.userID = {$currentUser->userID} LEFT JOIN games_favorites favorites ON games.gameID = favorites.gameID AND favorites.userID = {$currentUser->userID} WHERE (players.gameID IS NOT NULL OR favorites.gameID IS NOT NULL) AND games.retired IS NULL ORDER BY games.start DESC LIMIT 6")->fetchAll();
+			$games = $mysql->query("SELECT games.gameID, games.title, games.forumID, IF(players.isGM, players.isGM, FALSE) isGM, IF(players.userID, TRUE, FALSE) isPlayer FROM games LEFT JOIN players ON games.gameID = players.gameID AND players.userID = {$currentUser->userID} LEFT JOIN games_favorites favorites ON games.gameID = favorites.gameID AND favorites.userID = {$currentUser->userID} WHERE (players.gameID IS NOT NULL OR favorites.gameID IS NOT NULL) AND games.retired IS NULL ORDER BY games.start DESC LIMIT 10")->fetchAll();
 
 			foreach ($games as &$game) {
 				foreach(['gameID', 'forumID'] as $intKey) {
