9690d2ead095fb8947f5923d03f6ffad225d322e
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-09-25 15:55:43 -0400

    Create privacy policy

---
+++ privacy.php
@@ -0,0 +1,58 @@
+<?	require_once(FILEROOT.'/header.php'); ?>
+		<div class="flexWrapper">
+			<div class="mainColumn fullWidth">
+				<h1 class="headerbar">Privacy Policy</h1>
+				<div hb-margined>
+					<p>Gamers' Plane respects your privacy. As a solo project to bring gamers together, we collect the minimum of private data, and do not disclose any of it, save where it is collected directly by a third party.</p>
+
+					<h2>The Public and Collaborative Nature of the Projects and Games</h2>
+
+					<p>All data added by a user on Gamers' Plane after account creation are considered public. Any data added is identified publicly by the author's username. All contributions made to Gamers' Plane, and all publicly available information about those contributions, remain the property of the author and are irrevocably licensed and may be freely copied, quoted, reused and adapted by Gamers' Plane.</p>
+
+					<h2>Cookies</h2>
+					<p>Gamers' Plane only creates cookies to track if a user is logged in or not. Third party services Gamers' Plane utilizes may create their own cookies.</p>
+
+					<h2>Data Collected</h2>
+					<ul>
+						<li>E-mail address</li>
+						<li>Browser settings that are sent to Gamers' Plane's server(s)</li>
+						<li>Any information entered into the user profile</li>
+					</ul>
+					<p>The user's email address is the only mandatory data required to register an account with the site. Any other data provided is optional.</p>
+
+					<h2>How is your data used?</h2>
+					<p>Your email is used only to associate a user with an account and to communicate with the user as per the preferences chosen as per their user profile. Any other data collected is used to better serve the user and/or for inter-user communication.</p>
+					<p>No user data is disclosed with any external service, except where it is collected directly by a third party service.</p>
+
+					<h2>Third Party Services</h2>
+					<p>Gamers' Plane uses the following third party services:</p>
+					<ul>
+						<li>Google Analytics - Google Analytics is used to track user behavior. <a href="https://www.google.com/intl/en/policies/privacy/" target="_blank">More Information</a></li>
+						<li>Amazon Associate Links - Amazon Associate Links are used to direct users to Amazon for exploring and providing the option to purchase products. <a href="https://aws.amazon.com/privacy/" target="_blank">More Information</a></li>
+					</ul>
+
+					<h2>System logs</h2>
+					<p>Gamers' Plane tracks all user activity on the site and logs it to a system log. Logs may be kept for up to 30 days. These logs contain information such as the browser and IP address (and other data).</p>
+
+					<h2>Data Retention</h2>
+					<p>Needed for the proper functioning of the site and security, user data essential to the function of a user's account will be held in perpeuity. Any other data can be cleared by the user or by sending an email to <a href="mailto:contact@gamersplane.com">contact@gamersplane.com</a>.</p>
+
+					<h2>Information not contained in this policy</h2>
+					<p>More details concerning the collection or processing of Personal Data may be requested from the Owner at any time. Please see the contact information at the end of this document.</p>
+
+					<h2>How “Do Not Track” requests are handled</h2>
+					<p>This website does not support “Do Not Track” requests.</p>
+					<p>To determine whether any of the third-party services it uses honor the “Do Not Track” requests, please read their privacy policies.</p>
+
+					<h2>COPPA (Children Online Privacy Protection Act)</h2>
+					<p>When it comes to the collection of personal information from children under the age of 13 years old, the Children's Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, United States' consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children's privacy and safety online. We do not specifically market to children under the age of 13 years old.</p>
+
+					<h2>Changes to this Privacy Policy</h2>
+					<p>Gamers' Plane reserves the right to amend this Privacy Policy from time to time to reflect changes in the law, our data collection and data use practices, the features of Gamers' Plane's services or advances in technology. Please check this page periodically for changes. If we make a change to this Privacy Policy that, in our sole discretion, is material, we will notify you by posting notice of these changes in this Privacy Policy.</p>
+
+					<h2>Contact Information</h2>
+					<p>If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:contact@gamersplane.com">contact@gamersplane.com</a>.</p>
+				</div>
+			</div>
+		</div>
+<?	require_once(FILEROOT.'/footer.php'); ?>
