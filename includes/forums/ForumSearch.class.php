bffea7a138c627d051d1a44ab044f8287ac4caad
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-11-01 15:06:52 -0400

    Bugs left in the forum style fix

--- includes/forums/ForumSearch.class.php
+++ includes/forums/ForumSearch.class.php
@@ -142,7 +142,7 @@
 				<div class="td numPosts"># of Posts</div>
 				<div class="td lastPost">Last Post</div>
 			</div>
-			<div class="sudoTable forumList hbdMargined">
+			<div class="sudoTable forumList">
 <?
 			if (sizeof($this->results)) { foreach ($this->results as $result) {
 				$forumIcon = $result->lastPostID > $this->forumManager->getForumProperty($result->forumID, 'markedRead') && $result->lastPostID > $result->lastRead?'new':'old';
@@ -173,12 +173,13 @@
 ?>
 							<a href="/forums/thread/<?=$result->threadID?>/?view=lastPost#lastPost"><img src="/images/downArrow.png" title="Last post" alt="Last post"></a>
 						</div>
-						<a class="threadTitle" href="/forums/thread/<?=$result->threadID?>/"><?=$result->title?></a><br>
+						<a class="threadTitle" href="/forums/thread/<?=$result->threadID?>/"><?=$result->title?></a>
 						<span class="threadAuthor">by <a href="/user/<?=$result->authorID?>/" class="username"><?=$result->username?></a> in <a href="/forums/<?=$result->forumID?>/"><?=$result->forum?></a> on <span class="convertTZshort"><?=date('M j, Y g:i a', strtotime($result->datePosted))?></span></span>
 					</div>
 					<div class="td numPosts"><?=$result->postCount?></div>
 					<div class="td lastPost">
-					<a href="/forums/<?=$result->forumID?>/" class="forumDisplay"><?=$result->forum?></a><a href="/user/<?=$result->lp_authorID?>/" class="username"><?=$result->lp_username?></a><br><span class="convertTZshort"><?=date('M j, Y g:i a', strtotime($result->lp_datePosted))?></span>
+						<a href="/forums/<?=$result->forumID?>/" class="forumDisplay"><?=$result->forum?></a><a href="/user/<?=$result->lp_authorID?>/" class="username"><?=$result->lp_username?></a>
+						<span class="convertTZshort"><?=date('M j, Y g:i a', strtotime($result->lp_datePosted))?></span>
 					</div>
 				</div>
 <?			} } else { ?>
