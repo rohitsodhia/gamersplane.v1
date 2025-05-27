3b315586e4fc39353c5d8c4138ede5bc769d01a3
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-05-27 12:51:09 -0400

    Feature/list bbcode (#176)
    * Move email containers to a prod compose file
    
    * Rename docker-compose files to compose for modernity
    
    * Update table structures for initial load
    
    * Add list bbcodes back
    
    * Setup styles to allow bbcode lists
    
    * Bump css versions
    
    * Remove compose override example
    
    * Enhance lists to allow for start position and types
    
    * Allow for start in non numberic lists
    
    * Made code easier to read
    
    * Created newline regex pattern
    
    * Implemented list with adam's recursive regex method
    
    * Set list margin left
    
    * Move function out of function
    
    * Adjust list padding

--- javascript/markItUp/markitup.bbcode-parser.php
+++ javascript/markItUp/markitup.bbcode-parser.php
@@ -75,38 +75,49 @@ function splitByHeader($title, $text, $cssClass, $collectionData=''){
  * $callback - function to perform the replacement.  This is passed the tag found, the inner text, and the matches
  */
 function nestedReplace($text, $openTagNeedle, $closeTagNeedle, $callback, $options){
-    $replacementMade=true;
-    $searchFromPos=0;
-    $closeTagLen=strlen($closeTagNeedle);
+    $replacementMade = true;
+    $searchFromPos = 0;
+    $closeTagLen = strlen($closeTagNeedle);
 
-    do{
-        $replacementMade=false;
-        if(preg_match($openTagNeedle, $text, $matches, PREG_OFFSET_CAPTURE,$searchFromPos)) {
+    do {
+        $replacementMade = false;
+        if (preg_match($openTagNeedle, $text, $matches, PREG_OFFSET_CAPTURE, $searchFromPos)) {
             $foundTag = $matches[0][0];
             $openTagStart = $matches[0][1];
-			$openTagEnd=$openTagStart+strlen($foundTag);
-            $nextOpenTagPos=-1;
+			$openTagEnd = $openTagStart + strlen($foundTag);
+            $nextOpenTagPos = -1;
 
-            if(preg_match($openTagNeedle, $text, $matchesNext, PREG_OFFSET_CAPTURE,$openTagEnd)){
-                $nextOpenTagPos=$matchesNext[0][1];		//find the next opening tag
+            if (preg_match($openTagNeedle, $text, $matchesNext, PREG_OFFSET_CAPTURE, $openTagEnd)) {
+                $nextOpenTagPos = $matchesNext[0][1];		//find the next opening tag
             }
-            $nextCloseTag=strpos($text,$closeTagNeedle,$openTagEnd);
+			if ($closeTagNeedle[0] == '/') {
+				$nextCloseTag = preg_match($closeTagNeedle, $text, $closeMatches, PREG_OFFSET_CAPTURE, $openTagEnd);
+				error_log($nextCloseTag);
+			} else {
+				$nextCloseTag = strpos($text, $closeTagNeedle, $openTagEnd);
+			}
 
-            if($nextCloseTag!==false){
-                if($nextCloseTag<$nextOpenTagPos || $nextOpenTagPos==-1){
+            if ($nextCloseTag !== false) {
+                if ($nextCloseTag < $nextOpenTagPos || $nextOpenTagPos == -1) {
 					//found a closing tag before the next opening tag
-					$text=substr($text,0,$openTagStart).$callback($matches,substr($text,$openTagEnd,$nextCloseTag-$openTagEnd),$options).substr($text,$nextCloseTag+$closeTagLen);
-                    $replacementMade=true;
-                    $searchFromPos=$openTagEnd;
+					$text = substr($text, 0, $openTagStart).
+						$callback(
+							$matches,
+							substr($text, $openTagEnd, $nextCloseTag - $openTagEnd),
+							$options).
+						substr($text, $nextCloseTag + $closeTagLen);
+                    $replacementMade = true;
+                    $searchFromPos = $openTagEnd;
                 } else {
 					//found an opening tag before the next closing tag - nesting
-					$restOfText=substr($text,$openTagEnd);
-                    $text=substr($text,0,$openTagEnd).nestedReplace($restOfText,$openTagNeedle, $closeTagNeedle, $callback, $options);
-                    $replacementMade=true;
+					$restOfText = substr($text,$openTagEnd);
+                    $text = substr($text, 0, $openTagEnd).
+						nestedReplace($restOfText, $openTagNeedle, $closeTagNeedle, $callback, $options);
+                    $replacementMade = true;
                 }
             }
         }
-    }while($replacementMade);
+    } while ($replacementMade);
 
     return $text;
 }
@@ -129,12 +140,29 @@ function gpClassFormatter($matches,$innerText,$tag){
 	return "<{$tag} class=\"userColor ".implode(' ',$classes)."\">{$innerText}</{$tag}>";
 }
 
+function listTagBuilder($matches, $text, $options) {
+	// error_log(htmlspecialchars($text)); exit();
+	if (sizeof($matches) == 1) {
+		return "<ul>{$text}</ul>";
+	} elseif (is_numeric($matches[1][0])) {
+		return "<ol start=\"{$matches[1][0]}\">{$text}</ol>";
+	} else {
+		if (strpos($matches[1][0], ',') !== false) {
+			$parts = explode(',', $matches[1][0]);
+			return "<ol type=\"{$parts[0]}\" start=\"{$parts[1]}\">{$text}</ol>";
+		}
+		return "<ol type=\"{$matches[1][0]}\">{$text}</ol>";
+	}
+}
+
 function BBCode2Html($text) {
 	$text = trim($text);
 
 	//iOS uses smart quotes.  Replace those as they might be tag attributes.
 	$text = str_replace('“', '"', $text);
 	$text = str_replace('”', '"', $text);
+
+	$newlinePattern = "(?:\r\n|\r|\n)";
 
 	// BBCode [code]
 	if (!function_exists('escape')) {
@@ -209,6 +237,16 @@ function BBCode2Html($text) {
 	$scriptingAttackRegex = '/\[(.*?)\].*?javascript:.*?\[\/\1\]/';
 	$text = preg_replace($scriptingAttackRegex, '', $text);
 
+	// Lists
+	// $text = preg_replace_callback(
+	// 	'/\[list(?:\=((?:[aAiI](?:,\s?\d+)?)|\d+))?\]\s*(.*?)\[\/list\](?:\\r\\n|\\r|\\n)?/ms',
+	// 	function ($matches) {
+	// 	},
+	// 	$text
+	// );
+
+	$text = nestedReplace($text, "/\[list(?:\=((?:[aAiI](?:,\s?\d+)?)|\d+))?\]\s+/", "[/list]", 'listTagBuilder', []);
+
 	// Smileys to find...
 /*	$in = array( 	 ':)',
 					 ':D',
@@ -227,53 +265,56 @@ function BBCode2Html($text) {
 	);
 	$text = str_replace($in, $out, $text);
 */
+	error_log($text);
 	// BBCode to find...
-	$in = array( 	 '/\[b\](.*?)\[\/b\]/ms',
-					 '/\[i\](.*?)\[\/i\]/ms',
-					 '/\[u\](.*?)\[\/u\]/ms',
-					 '/\[s\](.*?)\[\/s\]/ms',
-					 "/[\r\n]*\[linebreak\][\r\n]*/",
-					 '/\[img\](.*?)\[\/img\]/ms',
-					 '/\[email\](.*?)\[\/email\]/ms',
-					 '/\[size\="?(.*?)"?\](.*?)\[\/size\]/ms',
-					 '/\[color\="?(.*?)"?\](.*?)\[\/color\]/ms',
-					 '/\[zoommap\="?(.*?)"?\](.*?)\[\/zoommap\]/ms',
-//					 '/\[list\=(.*?)\](.*?)\[\/list\]/ms',
-//					 '/\[list\](.*?)\[\/list\]/ms',
-//					 '/\[\*\]\s?(.*?)\n/ms',
-					"/[\r\n]*\[ooc\](.*?)\[\/ooc\][\r\n]*/ms",
-					"/[\r\n]*\[spoiler=\"?(.*?)\"?\](.*?)\[\/spoiler\][\r\n]*/ms",
-					"/[\r\n]*\[spoiler\](.*?)\[\/spoiler\][\r\n]*/ms",
-					"/\[youtube\]https:\/\/youtu.be\/(.*?)\[\/youtube\]/ms",
-					 "/[\r\n]*\[2column\][ \t\r\n]*(.*?)[ \t\r\n]*\[\/2column\][\r\n]*/ms",
-					 "/[\r\n]*\[3column\][ \t\r\n]*(.*?)[ \t\r\n]*\[\/3column\][\r\n]*/ms",
-					 "/[\r\n]*\[col\][ \t\r\n]*(.*?)[ \t\r\n]*\[\/col\][\r\n]*/ms",
-					 "/[\r\n]*\[style\](.*?)\[\/style\][\r\n]*/ms",
-					 "/\[npc=\"?(.*?)\"?\](.*?)\[\/npc\]*/ms",
+	$in = array(
+		'/\[b\](.*?)\[\/b\]/ms',
+		'/\[i\](.*?)\[\/i\]/ms',
+		'/\[u\](.*?)\[\/u\]/ms',
+		'/\[s\](.*?)\[\/s\]/ms',
+		"/[\r\n]*\[linebreak\]{$newlinePattern}*/",
+		'/\[img\](.*?)\[\/img\]/ms',
+		'/\[email\](.*?)\[\/email\]/ms',
+		'/\[size\="?(.*?)"?\](.*?)\[\/size\]/ms',
+		'/\[color\="?(.*?)"?\](.*?)\[\/color\]/ms',
+		'/\[zoommap\="?(.*?)"?\](.*?)\[\/zoommap\]/ms',
+		"/<\/ol>{$newlinePattern}/ms",
+		"/<\/ul>{$newlinePattern}/ms",
+		"/\[\*\]\s?(.*?){$newlinePattern}/m",
+		"/{$newlinePattern}*\[ooc\](.*?)\[\/ooc\]{$newlinePattern}*/ms",
+		"/{$newlinePattern}*\[spoiler=\"?(.*?)\"?\](.*?)\[\/spoiler\]{$newlinePattern}*/ms",
+		"/{$newlinePattern}*\[spoiler\](.*?)\[\/spoiler\]{$newlinePattern}*/ms",
+		"/\[youtube\]https:\/\/youtu.be\/(.*?)\[\/youtube\]/ms",
+		"/{$newlinePattern}*\[2column\][ \t\r\n]*(.*?)[ \t\r\n]*\[\/2column\]{$newlinePattern}*/ms",
+		"/{$newlinePattern}*\[3column\][ \t\r\n]*(.*?)[ \t\r\n]*\[\/3column\]{$newlinePattern}*/ms",
+		"/{$newlinePattern}*\[col\][ \t\r\n]*(.*?)[ \t\r\n]*\[\/col\]{$newlinePattern}*/ms",
+		"/{$newlinePattern}*\[style\](.*?)\[\/style\]{$newlinePattern}*/ms",
+		"/\[npc=\"?(.*?)\"?\](.*?)\[\/npc\]*/ms",
 	);
 	// And replace them by...
-	$out = array(	 '<strong>\1</strong>',
-					 '<em>\1</em>',
-					 '<u>\1</u>',
-					 '<span style="text-decoration:line-through">\1</span>',
-					 '<hr>',
-					 '<img src="\1" alt="\1" class="usrImg">',
-					 '<a href="mailto:\1">\1</a>',
-					 '<span class="userSize" style="font-size:\1%">\2</span>',
-					 '<span class="userColor" style="color:\1">\2</span>',
-					 '<div class="zoommap" data-mapimage="\1" style="display:none">\2</div>',
-//					 '<ol start="\1">\2</ol>',
-//					 '<ul>\1</ul>',
-//					 '<li>\1</li>',
-					 '<blockquote class="oocText"><div>OOC:</div>\1</blockquote>',
-					 '<blockquote class="spoiler closed"><div class="tag">[ <span class="open">+</span><span class="close">-</span> ] \1</div><div class="hidden">\2</div></blockquote>',
-					 '<blockquote class="spoiler closed"><div class="tag">[ <span class="open">+</span><span class="close">-</span> ] Spoiler</div><div class="hidden">\1</div></blockquote>',
-					 '<div class="youtube_bb"><iframe src="https://www.youtube.com/embed/\1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
-					 '<div class="layout-columns-2">\1</div>',
-					 '<div class="layout-columns-3">\1</div>',
-					 '<div class="layout-column">\1</div>',
-					 '<div class="style" style="display:none;">\1</div>',
-					 '<div class="inlineNpcPrefix"></div><div class="inlineNpc"><img class="inlineNpcAvatar" src="\2"/><div class="inlineNpcName">\1</div></div>',
+	$out = array(
+		'<strong>\1</strong>',
+		'<em>\1</em>',
+		'<u>\1</u>',
+		'<span style="text-decoration:line-through">\1</span>',
+		'<hr>',
+		'<img src="\1" alt="\1" class="usrImg">',
+		'<a href="mailto:\1">\1</a>',
+		'<span class="userSize" style="font-size:\1%">\2</span>',
+		'<span class="userColor" style="color:\1">\2</span>',
+		'<div class="zoommap" data-mapimage="\1" style="display:none">\2</div>',
+		'</ol>',
+		'</ul>',
+		'<li>\1</li>',
+		'<blockquote class="oocText"><div>OOC:</div>\1</blockquote>',
+		'<blockquote class="spoiler closed"><div class="tag">[ <span class="open">+</span><span class="close">-</span> ] \1</div><div class="hidden">\2</div></blockquote>',
+		'<blockquote class="spoiler closed"><div class="tag">[ <span class="open">+</span><span class="close">-</span> ] Spoiler</div><div class="hidden">\1</div></blockquote>',
+		'<div class="youtube_bb"><iframe src="https://www.youtube.com/embed/\1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
+		'<div class="layout-columns-2">\1</div>',
+		'<div class="layout-columns-3">\1</div>',
+		'<div class="layout-column">\1</div>',
+		'<div class="style" style="display:none;">\1</div>',
+		'<div class="inlineNpcPrefix"></div><div class="inlineNpc"><img class="inlineNpcAvatar" src="\2"/><div class="inlineNpcName">\1</div></div>',
 	);
 
 	$text = preg_replace($in, $out, $text);
