15c202fb14f6f2f728a9a013031994755a596d8d
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-06-02 18:15:32 -0400

    Update backup charset to utf8mb4 (#183)

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 export $(grep -v '^#' .env | xargs -d '\n')
 filename=$(date +'%Y%m%d_%H%M%S')
 
-docker-compose exec -T mysql bash -c "mysqldump --user=$MYSQL_USERNAME --password='$MYSQL_PASSWORD' --default-character-set=utf8 --ignore-table=$MYSQL_DATABASE.forums_readData_forums_c --ignore-table=$MYSQL_DATABASE.forums_readData_newPosts $MYSQL_DATABASE > /tmp/$filename.sql"
+docker-compose exec -T mysql bash -c "mysqldump --user=$MYSQL_USERNAME --password='$MYSQL_PASSWORD' --default-character-set=utf8mb4 --ignore-table=$MYSQL_DATABASE.forums_readData_forums_c --ignore-table=$MYSQL_DATABASE.forums_readData_newPosts $MYSQL_DATABASE > /tmp/$filename.sql"
 mysql_container=$(docker ps | grep -E 'mysql' | awk '{ print $1 }')
 docker cp $mysql_container:/tmp/$filename.sql $BACKUP_DIR/mysql/
 zstd $BACKUP_DIR/mysql/$filename.sql
